e5c865b8649a89271914d4347863df66ae2681fb
Federico E. Benelli <fedebenelli@outlook.com>
2023-08-08 16:50:17 -0300

    frmat

--- src/models/models.f90
+++ src/models/models.f90
@@ -2,6 +2,6 @@ module yaeos_models
     use yaeos_ar_models
 
     private
-    public :: residual_helmholtz, set_ar_function
+    public :: residual_helmholtz, set_ar_function, ar_fun
 
 end module
