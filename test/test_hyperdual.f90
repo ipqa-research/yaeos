e1f8ba42b5390f0e2782d8313c5a9d3744cd0f78
Federico E. Benelli <fedebenelli@outlook.com>
2023-08-08 16:48:38 -0300

    feat(test): Test hyperdual sqrt and mul behaviour

---
+++ test/test_hyperdual.f90
@@ -0,0 +1,27 @@
+program main
+    use yaeos_autodiff
+    use yaeos_constants, only: pr
+    implicit none
+    type(hyperdual) :: x, y, z
+
+
+    x = 2.0_pr
+    y = 3.0_pr
+
+    x%f1 = 1
+    y%f2 = 1
+
+    z = f(x, y)
+
+    if (abs(z%f0 - 2.4494897427831779) > 1e-5) call exit(1)
+    if (abs(z%f1 - 0.61237243569579447) > 1e-5) call exit(1)
+    if (abs(z%f2 - 0.40824829046386302) > 1e-5) call exit(1)
+    if (abs(z%f12 - 0.10206207261596575) > 1e-5) call exit(1)
+
+contains
+
+    function f(x, y)
+        type(hyperdual) :: x, y, f
+        f = sqrt(x*y)
+    end function
+end program
