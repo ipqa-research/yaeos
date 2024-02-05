d5c0fc522837eabd6b81cad709c0e6de7297b145
Federico E. Benelli <fedebenelli@outlook.com>
2024-02-05 13:50:43 -0300

    docs

--- src/models/models.f90
+++ src/models/models.f90
@@ -1,5 +1,10 @@
 module yaeos_models
+    !! Ya_EoS thermodynamic models
+
+    ! Residual Helmholtz Models
     use yaeos_models_ar, only: ArModel
+
+    ! Cubic EoS models
     use yaeos_models_ar_genericcubic, only: CubicEoS, GenericCubic_Ar
     use yaeos_models_ar_cubic_alphas, only: AlphaSoave
     use yaeos_models_ar_genericcubic_quadratic_mixing, only: QMR
