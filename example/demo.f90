f3f31e74dee3e7938e43d032af1557a61df3a092
Federico E. Benelli <fedebenelli@outlook.com>
2024-02-13 03:46:13 -0300

    demo runner

--- example/demo.f90
+++ example/demo.f90
@@ -2,8 +2,11 @@ program examples
     use bench, only: benchmarks
     use hyperdual_pr76, only: adiff_pr76
     use flashing, only: run_flashes
+    use TapeRobinson, only: run_tape => main
 
-    ! call benchmarks
+    call run_tape
+    call benchmarks
     ! call adiff_pr76
-    call run_flashes
+    ! call run_flashes
+    ! call run_tape
 end program
