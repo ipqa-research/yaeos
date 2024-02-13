6e8b7e1a223ffc6cd6a8c3816c9eff1811a0c133
Federico E. Benelli <fedebenelli@outlook.com>
2024-02-13 03:45:26 -0300

    add tapenade to benchs

--- example/benchmark.f90
+++ example/benchmark.f90
@@ -1,6 +1,7 @@
 module bench
     use yaeos, only: pr, R, Substances, AlphaSoave, CubicEoS, fugacity_vt, QMR, PengRobinson76, ArModel
     use hyperdual_pr76, only: PR76, setup_adiff_pr76 => setup
+    use TapeRobinson, only: setup_taperobinson => setup_model, tape_model => model, ArModelTapenade
     implicit none
 
     real(pr), allocatable :: z(:), tc(:), pc(:), w(:), kij(:,:), lij(:,:)
@@ -37,10 +38,12 @@ contains
             model = PengRobinson76(tc, pc, w, kij, lij)
         case ("Adiff PR76")
             model = setup_adiff_pr76(tc, pc, w, kij, lij)
+        case ("Tape PR76")
+            call setup_taperobinson(tc, pc, w, kij, lij)
+            model = tape_model
         end select
 
         v = 1.0_pr
-        ! t = maxval(model%components%tc)
         t = 150._pr
 
         if (dn) then
@@ -51,30 +54,47 @@ contains
     end subroutine
 
     subroutine benchmarks
+        integer :: n=30
+        logical :: allderivs=.false.
+
+        ! "Analytic PR76"
+        ! "Adiff PR76"
+        ! "Tape PR76"
+
+        call run_bench(n, allderivs, "Analytic PR76")
+        call run_bench(n, allderivs, "Tape PR76")
+        call run_bench(n, allderivs, "Adiff PR76")
+    end subroutine
+
+    subroutine run_bench(nmax, all_derivs, eos)
         integer, parameter :: nevals=1e3
-        integer :: i, n
+        integer :: nmax
+        logical :: all_derivs
+        character(len=*), intent(in) :: eos
         real(8) :: time, std, mean
         real(8) :: et, st
+        integer :: i, n
+        time = 0
+        std = 0
+        mean = 0
 
+        print *, "running: ", eos
 
-        do n=1,30
-            time = 0
-            std = 0
-            mean = 0
+        do n=1,nmax
             do i=1,nevals
                 call cpu_time(st)
-                    call yaeos_run(n, .true., "Analytic PR76")
+                    call yaeos_run(n, all_derivs, eos)
                 call cpu_time(et)
 
                 time = (et-st)*1e6
                 mean = mean + time
                 std = std + time**2
             end do
-
             mean = mean/nevals
             std = sqrt(1._pr/(nevals-1) * (std - nevals*mean**2))
 
             print *, n, mean, std
         end do
+
     end subroutine
 end module
