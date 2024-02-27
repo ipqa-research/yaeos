40f88c20936141fc228c030baaab2522c48e1543
fedebenelli <fedebenelli@outlook.com>
2024-02-27 01:01:55 +0000

    Deploying to gh-pages from @ ipqa-research/yaeos@c432bafc0babed9d2667fda931da8d7d86931cbf 🚀

--- src/yaeos.f90
+++ src/yaeos.f90
@@ -1,8 +1,11 @@
 module yaeos
+   !! Yet Another Equation-Of-State (library)
+   !!
+   !!
    use yaeos_constants
    use yaeos_substance
    use yaeos_models
    use yaeos_thermoprops
    use yaeos_equilibria
-   character(len=*), parameter :: version="0.1.0b"
+   character(len=*), parameter :: version="0.1.0b2"
 end module
