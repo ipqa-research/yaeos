cf5d797f11b6f5958ffadfe3ca8ccdb5e7ad14ca
Federico E. Benelli <fedebenelli@outlook.com>
2024-03-04 16:29:55 -0300

    bootstrap

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update && apt-get install -y wget
 wget https://raw.githubusercontent.com/ipqa-research/fortran-setup/main/bootstrap_fortran.sh
-bash bootstrap_fortran.sh
+printf "n\nn\ny\ny" | bash bootstrap_fortran.sh
