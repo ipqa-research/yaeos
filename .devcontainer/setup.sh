ea5ccaa427e4b10e12f556cf5c091a0ca4614937
Federico E. Benelli <fedebenelli@outlook.com>
2024-03-04 16:22:06 -0300

    doesnt need sudo

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install wget
+apt-get update && apt-get install wget
 wget https://raw.githubusercontent.com/ipqa-research/fortran-setup/main/bootstrap_fortran.sh
 bash bootstrap_fortran.sh
