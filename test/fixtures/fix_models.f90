ced15ca717ab969d8a68a304160bd91086ac6173
fedebenelli <fedebenelli@outlook.com>
2024-03-10 14:52:35 -0300

    testsss

--- test/fixtures/fix_models.f90
+++ test/fixtures/fix_models.f90
@@ -8,7 +8,6 @@ contains
         integer, parameter :: n=2
         real(pr) :: tc(n), pc(n), w(n)
         real(pr) :: kij(n, n), lij(n, n)
-        ! z = [0.3_pr, 0.7_pr]
         tc = [190._pr, 310._pr]
         pc = [14._pr, 30._pr]
         w = [0.001_pr, 0.03_pr]
@@ -18,6 +17,38 @@ contains
 
 
         eos = PengRobinson76(tc, pc, w, kij, lij)
+    end function
+    
+    type(CubicEoS) function binary_PR78() result(eos)
+        use yaeos, only: PengRobinson78
+        integer, parameter :: n=2
+        real(pr) :: tc(n), pc(n), w(n)
+        real(pr) :: kij(n, n), lij(n, n)
+        tc = [190._pr, 310._pr]
+        pc = [14._pr, 30._pr]
+        w = [0.001_pr, 0.03_pr]
+
+        kij = reshape([0., 0.1, 0.1, 0.], [n,n]) 
+        lij = kij / 2 
+
+
+        eos = PengRobinson78(tc, pc, w, kij, lij)
+    end function
+    
+    type(CubicEoS) function binary_SRK() result(eos)
+        use yaeos, only: SoaveRedlichKwong
+        integer, parameter :: n=2
+        real(pr) :: tc(n), pc(n), w(n)
+        real(pr) :: kij(n, n), lij(n, n)
+        tc = [190._pr, 310._pr]
+        pc = [14._pr, 30._pr]
+        w = [0.001_pr, 0.03_pr]
+
+        kij = reshape([0., 0.1, 0.1, 0.], [n,n]) 
+        lij = kij / 2 
+
+
+        eos = SoaveRedlichKwong(tc, pc, w, kij, lij)
     end function
 
 end module
--- test/test_runner.f90
+++ test/test_runner.f90
@@ -1,14 +1,15 @@
 program tester
-    use, intrinsic :: iso_fortran_env, only : error_unit
-    use testdrive, only : run_testsuite, new_testsuite, testsuite_type
+    use, intrinsic :: iso_fortran_env, only: error_unit
+    use testdrive, only: run_testsuite, new_testsuite, testsuite_type
 
     use test_legacy, only: suite_legacy => collect_suite
     use test_cubic_alphas, only: suite_alphas => collect_suite
+    use test_cubic_implementations, only: suite_implementations => collect_suite
     use test_thermoprops, only: suite_thermoprops => collect_suite
     use test_flash, only: suite_flash => collect_suite
 
-    use stdlib_ansi, only : fg_color_green, fg_color_red, operator(//), style_reset
-    
+    use stdlib_ansi, only: fg_color_green, fg_color_red, operator(//), style_reset
+
     implicit none
 
     integer :: stat, is
@@ -18,22 +19,23 @@ program tester
     stat = 0
 
     testsuites = [ &
-        new_testsuite("legacy", suite_legacy ),  &
+        new_testsuite("legacy", suite_legacy), &
         new_testsuite("Alphas", suite_alphas), &
+        new_testsuite("Cubic EoS", suite_implementations), &
         new_testsuite("Thermoprops", suite_thermoprops), &
         new_testsuite("Flash", suite_flash) &
-    ]
+        ]
 
     do is = 1, size(testsuites)
-        write(error_unit, fmt) "Testing:", testsuites(is)%name
+        write (error_unit, fmt) "Testing:", testsuites(is)%name
         call run_testsuite(testsuites(is)%collect, error_unit, stat)
     end do
 
     if (stat > 0) then
-        write(error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
+        write (error_unit, '(i0, 1x, a)') stat, "test(s) failed!"
         error stop
-    else 
-        write(*, *) fg_color_green // "All tests run!" // style_reset
+    else
+        write (*, *) fg_color_green//"All tests run!"//style_reset
     end if
 
 end program tester
