7a8d125cc4932b65b7393a82e89c9be2689fc40e
salvador <salvadorebrandolin@mi.unc.edu.ar>
2024-05-05 20:29:32 -0300

    numeric_ar_derivatives

---
+++ src/consistency/consistency.f90
@@ -0,0 +1,8 @@
+module yaeos_consistency
+    !! TODO: document the module.
+    ! Consistency test for ArModels
+    use yaeos_consistency_armodel
+
+    ! TODO: Consistency test for GeModels
+    ! use yaeos_consistency_gemodel
+end module yaeos_consistency
