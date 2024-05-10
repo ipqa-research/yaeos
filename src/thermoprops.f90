e32d8c85e456d58f9103287a78b1cc4c22296ca0
Federico E. Benelli <fedebenelli@outlook.com>
2024-05-10 16:28:31 -0300

    Warning was too exagerated for very small pressures

--- src/thermoprops.f90
+++ src/thermoprops.f90
@@ -83,7 +83,7 @@ contains
       call volume(eos, n, P, T, V_in, root_type)
       call fugacity_vt(eos, n, v_in, T, P_in, lnphip, dlnphidp, dlnphidt, dlnphidn)
       if(present(v)) v = v_in
-      if(abs(P_in - p)/p > 1e-5) then
+      if(abs(P_in - p) > 1e-2) then
          write(error_unit, *) "WARN: possible bad root solving: ", p_in, p
       end if
    end subroutine fugacity_tp
