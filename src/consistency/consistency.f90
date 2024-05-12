63b166f6a328900dbe34046f6bba3e493ca5013b
salvador <salvadorebrandolin@mi.unc.edu.ar>
2024-05-12 18:17:34 -0300

    consistency module and tests for all cubic models

---
+++ src/consistency/consistency.f90
@@ -0,0 +1,10 @@
+module yaeos_consistency
+   !! Tools to evalaute the consistency of Ar and Ge models. This module also
+   !! provides subroutines for numeric evaluations of Ar and Ge derivatives from
+   !! central finite differences.
+   ! Consistency test for ArModels
+   use yaeos_consistency_armodel
+
+   ! TODO: Consistency test for GeModels
+   ! use yaeos_consistency_gemodel
+end module yaeos_consistency
