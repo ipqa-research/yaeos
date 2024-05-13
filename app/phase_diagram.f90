fbc81ddab6ff5fe11b932e5aabefc7c8b686ba9c
Federico E. Benelli <fedebenelli@outlook.com>
2024-05-13 12:11:14 -0300

    test

--- app/phase_diagram.f90
+++ app/phase_diagram.f90
@@ -13,23 +13,27 @@ program phase_diagram
    real(pr) :: n(nc), tc(nc), pc(nc), w(nc), kij(nc, nc), lij(nc, nc)
 
    ! Methane/ Butane mixture
-   n = [0.4, 0.6]                      ! Composition
-   tc = [190.564, 425.12]              ! Critical temperatures
-   pc = [45.99, 37.96]                 ! Critical pressures
-   w = [0.0115478, 0.200164]           ! Acentric factors
+   n = [0.1_pr, 0.9_pr]       ! Composition
+   tc = [190.564, 425.12]     ! Critical temperatures
+   pc = [45.99, 37.96]        ! Critical pressures
+   w = [0.0115478, 0.200164]  ! Acentric factors
 
-   kij = reshape([0., 0.1, 0.1, 0.], [nc,nc]) 
+   kij = 0
+   kij(1, 2) = 0.2
+   kij(2, 1) = kij(1, 2)
+
    lij = kij / 2 
    model = PengRobinson76(tc, pc, w, kij, lij)
 
-   ! Calculate a bubble point
-   bubble = saturation_pressure(model, n, 150._pr, kind="bubble")
-   print *, "# Initial bubble point"
-   print *, bubble%t, bubble%p
-
-   ! Calculate the whole phase envelope using the bubble point as an initial
-   ! state
-   envelope = pt_envelope_2ph(model, n, bubble)
+   ! Calculate a dew point
+   bubble = saturation_pressure(model, n, 150.0_pr, kind="bubble", p0=40._pr)
+   print *, bubble%T, bubble%p
+   
+   ! Calculate the whole phase envelope using the information from the converged
+   ! dew point
+   envelope = pt_envelope_2ph(&
+      model, n, bubble%y, T0=bubble%T, P0=bubble%p, delta_0=0.01_pr&
+   )
 
    print *, "# Envelope"
    print *, "T              P"
