a9a106e012a423b10422bab9256ee421aa1b3fee
fedebenelli <fedebenelli@outlook.com>
2024-06-04 23:46:01 -0300

    dx_to_dn from CallebBell, needs to be tested

--- src/math/math.f90
+++ src/math/math.f90
@@ -35,4 +35,30 @@ contains
       real(pr), intent(in) :: pred
       sq_error = ((exp - pred)/exp)**2
    end function sq_error
+
+   function dx_to_dn(x, dx) result(dn)
+      !! # dx_to_dn
+      !!
+      !! # Description
+      !! Convert the mole fraction derivatives of a quantity (calculated
+      !! so they do not sum to 1) to mole number derivatives (where the mole
+      !! fractions do sum to one). Requires the derivatives and the mole fractions
+      !! of the mixture. 
+      !! From ![https://chemicals.readthedocs.io/chemicals.utils.html?highlight=dxs_to_dns#chemicals.utils.dxs_to_dns](Chemicals (Python))
+      use yaeos__constants, only: pr
+      real(pr), intent(in) :: x(:)
+      real(pr), intent(in) :: dx(:)
+      real(pr) :: dn(size(x))
+
+      integer :: i, nc
+
+      nc = size(x)
+      dn = 0
+
+      forall(i=1:nc, j=1:nc, i /= j)
+         dn(i) = dn(i) - x(j) * dx(j)
+      end forall
+
+      dn = dx - dn
+   end function
 end module yaeos__math
