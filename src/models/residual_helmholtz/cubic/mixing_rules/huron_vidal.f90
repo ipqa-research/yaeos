949f6d3a7fbe39ed857661d9d8ff75176e89bace
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-06-14 16:59:40 -0300

    first hv draft

--- src/models/residual_helmholtz/cubic/mixing_rules/huron_vidal.f90
+++ src/models/residual_helmholtz/cubic/mixing_rules/huron_vidal.f90
@@ -1,8 +1,7 @@
 module yaeos__models_cubic_mixing_rules_huron_vidal
-   use yaeos__constants, only: pr
+   use yaeos__constants, only: pr, R
    use yaeos__models_ar_genericcubic, only: CubicMixRule
-   use yaeos__models_ar_cubic_quadratic_mixing, only: &
-      D1mix_constant, bmix_lij
+   use yaeos__models_ar_cubic_mixing_base, only: bmix_qmr
    use yaeos__models_ge, only: GeModel
    implicit none
 
@@ -11,22 +10,35 @@ module yaeos__models_cubic_mixing_rules_huron_vidal
       real(pr), private, allocatable :: bi(:)
       real(pr), private, allocatable :: B, dBi(:), dBij(:, :)
       class(GeModel), allocatable :: ge
+      real(pr) :: q
    contains
       procedure :: Bmix => BmixHV
       procedure :: D1Mix => D1Mix_constantHV
       procedure :: Dmix => DmixHV
    end type
 
+   interface HV
+      module procedure :: init
+   end interface
+
 contains
 
    type(HV) function init(ge, b, lij) result(mixrule)
       class(GeModel), intent(in) :: Ge
       real(pr), intent(in) :: b(:)
-      real(pr), intent(in) :: lij(:, :)
+      real(pr), optional, intent(in) :: lij(:, :)
+
+      integer :: i, nc
+
+      nc = size(b)
 
       mixrule%bi = b
-      mixrule%l = lij
       mixrule%Ge = ge
+      if (present(lij)) then
+        mixrule%l = lij
+      else
+        mixrule%l = reshape([(0, i=1,nc**2)], [nc,nc])
+      end if
    end function
 
    subroutine BmixHV(self, n, bi, B, dBi, dBij)
@@ -34,7 +46,7 @@ contains
       real(pr), intent(in) :: n(:)
       real(pr), intent(in) :: bi(:)
       real(pr), intent(out) :: B, dBi(:), dBij(:, :)
-      call bmix_lij(n, bi, self%l, b, dbi, dbij)
+      call bmix_qmr(n, bi, self%l, b, dbi, dbij)
    end subroutine
 
    subroutine DmixHV(self, n, T, &
@@ -47,7 +59,24 @@ contains
       real(pr), intent(out) :: D, dDdT, dDdT2, dDi(:), dDidT(:), dDij(:, :)
 
       real(pr) :: b, bi(size(n)), dbi(size(n)), dbij(size(n), size(n))
+      real(pr) :: Ge, GeT, GeT2, Gen(size(n)), GeTn(size(n)), Gen2(size(n), size(n))
+
+      real(pr) :: totn !! Total number of moles
+
+      totn = sum(n)
       call self%Bmix(n, self%bi, B, dBi, dBij)
+      call self%ge%excess_gibbs(n, T, Ge=Ge, GeT=GeT, GeT2=GeT2, Gen=Gen, GeTn=GeTn, Gen2=Gen2)
+
+      D = sum(n*ai/self%bi) + 1/self%q*(Ge + R*T*sum(n*log(B/(totn*self%bi))))
+      D = D*B
    end subroutine
 
+   subroutine D1Mix_constantHV(self, n, d1i, D1, dD1i, dD1ij)
+      class(HV), intent(in) :: self
+      real(pr), intent(in) :: n(:)
+      real(pr), intent(in) :: d1i(:)
+      real(pr), intent(out) :: D1
+      real(pr), intent(out) :: dD1i(:)
+      real(pr), intent(out) :: dD1ij(:, :)
+   end subroutine D1Mix_constantHV
 end module yaeos__models_cubic_mixing_rules_huron_vidal
