76160d443b909e1bc406d298039f27a4179b962b
fedebenelli <fedebenelli@outlook.com>
2024-06-26 12:34:49 -0300

    fitting of nrtl model inside MHV, to move outside later

--- app/fit.f90
+++ app/fit.f90
@@ -1,22 +1,114 @@
+module yaeos__fitting_fit_nrtl
+   use yaeos__constants, only: pr
+   use yaeos__fitting, only: FittingProblem
+   use yaeos__models, only: ArModel, NRTL, CubicEoS, MHV
+   use forsus, only: Substance
+   implicit none
+
+   integer, parameter :: nc = 2
+
+   type, extends(FittingProblem) :: FitMHVNRTL
+   contains
+      procedure :: get_model_from_X => model_from_X
+   end type
+
+contains
+
+   subroutine init_model(problem, sus)
+      use yaeos, only: R, ArModel, CubicEoS, PengRobinson78, RKPR, SoaveRedlichKwong
+      class(FitMHVNRTL), intent(in out) :: problem
+      type(Substance), intent(in) :: sus(2)
+      type(MHV) :: mixrule
+      type(NRTL) :: ge
+      real(pr) :: tc(nc), pc(nc), w(nc), vc(nc), zc(nc)
+      real(pr) :: a(nc, nc), b(nc, nc), c(nc, nc), bi(nc)
+
+      a=0; b=0; c=0
+
+      tc = sus%critical%critical_temperature%value
+      pc = sus%critical%critical_pressure%value/1e5
+      w = sus%critical%acentric_factor%value
+      vc = sus%critical%critical_volume%value
+      zc = pc*vc/(R*tc)
+
+      ge = NRTL(a, b, c)
+
+      ! problem%model = RKPR(tc, pc, w, zc)
+      allocate(CubicEoS :: problem%model)
+      problem%model = SoaveRedlichKwong(tc, pc, w)
+
+      associate(m => problem%model)
+         select type(m)
+         type is (CubicEoS)
+            bi = m%b
+            mixrule = MHV(ge, bi)
+            mixrule%q = -0.593
+            deallocate(m%mixrule)
+            m%mixrule = mixrule
+         end select
+      end associate
+   end subroutine init_model
+
+   function model_from_X(problem, X) result(model)
+      use yaeos, only: R, RKPR, PengRobinson78, ArModel, QMR, CubicEoS
+      use yaeos__models_ar_cubic_quadratic_mixing, only: RKPR_D1mix
+      real(pr), intent(in) :: X(:)
+      class(FitMHVNRTL), intent(in) :: problem
+      class(ArModel), allocatable :: model
+      type(NRTL) :: ge
+
+      real(pr) :: a(nc, nc), b(nc, nc), c(nc, nc)
+
+      a=0; b=0; c=0
+
+      a(1, 2) = x(1)
+      a(2, 1) = x(2)
+
+      b(1, 2) = x(3)
+      b(2, 1) = x(4)
+
+      c(1, 2) = x(5)
+      c(2, 1) = x(6)
+
+      ge = NRTL(a, b, c)
+
+      model = problem%model
+
+      select type(model)
+      class is (CubicEoS)
+         associate(mr => model%mixrule)
+            select type (mr)
+            class is (MHV)
+               mr%l(1, 2) = x(7)
+               mr%l(2, 1) = x(7)
+               mr%ge = ge
+               model%del1 = x(8:)
+               model%del2 = (1._pr - model%del1)/(1._pr + model%del1)
+            end select
+         end associate
+      end select
+   end function model_from_X
+end module
 
 program main
    !! Binary system parameter optimization
    use yaeos, only: EquilibriaState, pr, ArModel, PengRobinson78, CubicEoS, saturation_pressure
    use forsus, only: Substance, forsus_dir
    use yaeos__fitting, only: FittingProblem, fobj, optimize
-   use yaeos__fitting_fit_kij_lij, only: FitKijLij, init_model
-   integer, parameter :: nc=2
+   ! use yaeos__fitting_fit_kij_lij, only: FitKijLij, init_model
+   use yaeos__fitting_fit_nrtl, only: FitMHVNRTL, init_model
+   integer, parameter :: nc = 2, np=7 + nc
    integer :: i, infile, iostat
 
    type(EquilibriaState), allocatable :: exp_points(:)
    type(EquilibriaState) :: point
 
-   type(FitKijLij) :: prob
+   type(FitMHVNRTL) :: prob
    type(Substance) :: sus(2)
 
    class(ArModel), allocatable :: model
 
-   real(pr) :: T, P, x1, y1, kij, X(4), told, error
+   real(pr) :: T, P, x1, y1, kij, X(np), told, error
    character(len=14) :: kind
 
    ! ==========================================================================
@@ -26,51 +118,64 @@ program main
    sus(1) = Substance("nitrogen", only=["critical"])
    sus(2) = Substance("n-octane", only=["critical"])
 
-   allocate(exp_points(0))
-   open(newunit=infile, file="fit_case", iostat=iostat)
+   allocate (exp_points(0))
+   open (newunit=infile, file="fit_case", iostat=iostat)
    do
-      read(infile, *, iostat=iostat) kind, t, p, x1, y1
+      read (infile, *, iostat=iostat) kind, t, p, x1, y1
       if (iostat /= 0) exit
-      select case(kind)
-       case("bubble", "dew", "liquid-liquid")
-         point = EquilibriaState(&
-            kind=kind, T=T, P=P, x=[x1, 1-x1], y=[y1,1-y1], &
-            Vx=0._pr, Vy=0._pr, iters=0, beta=0._pr &
-            )
+      select case (kind)
+      case ("bubble", "dew", "liquid-liquid")
+         point = EquilibriaState( &
+                 kind=kind, T=T, P=P, x=[x1, 1 - x1], y=[y1, 1 - y1], &
+                 Vx=0._pr, Vy=0._pr, iters=0, beta=0._pr &
+                 )
       end select
       exp_points = [exp_points, point]
    end do
-   close(infile)
+   close (infile)
 
    ! ==========================================================================
    ! Setup optimization problem and call the optimization function
    ! --------------------------------------------------------------------------
    call init_model(prob, sus)
-   prob%fit_kij = .true.
-   prob%fit_lij = .true.
 
    prob%experimental_points = exp_points
 
-   X = [0.01, 0.01, 1., 1.]
-   prob%parameter_step = [0.01, 0.01, 0.1, -0.1]
+   X = 0
+   X(1:2) = [0.1, 0.3]
+   X(5:6) = [0.1, 0.2]
+   X(8:) = [1, 1]
+
+   ! X = [&
+   !    4.2479256344693399E-002, 0.18093681843489456, -4.4464361429422082E-003,&
+   !    -6.4352731108886688E-002, 7.8582006408860872E-002,  0.24011575038219565, 0.01& 
+   ! ]
+   ! X = [&
+   !    -3.6202219570888912, 0.43157772251067633, -0.44909362863508034, &
+   !    0.46870900745670463, -0.42525110258449428, 2.3435672739404647, &
+   !    0.21553218750038463]
+   
+   prob%parameter_step = [(0.5_pr, i=1,size(x))]
+   prob%solver_tolerance = 1e-7
+
    print *, "X0:", X
    error = optimize(X, prob)
    print *, "FO:", error
    print *, "Xf:", X
 
-   if (allocated(model)) deallocate(model)
+   if (allocated(model)) deallocate (model)
    model = prob%get_model_from_X(X)
 
    ! ==========================================================================
    ! Write out results and experimental values
    ! --------------------------------------------------------------------------
    told = exp_points(1)%T
-   do i=1, size(exp_points)
-      point = saturation_pressure(&
-         model, exp_points(i)%x, exp_points(i)%t, kind="bubble", &
-         p0=exp_points(i)%p, y0=exp_points(i)%y &
-         )
-      if (told /= point%t) write(*, "(/)")
+   do i = 1, size(exp_points)
+      point = saturation_pressure( &
+              model, exp_points(i)%x, exp_points(i)%t, kind="bubble", &
+              p0=exp_points(i)%p, y0=exp_points(i)%y &
+              )
+      if (told /= point%t) write (*, "(/)")
       print *, exp_points(i)%x(1), exp_points(i)%y(1), exp_points(i)%P, &
          point%x(1), point%y(1), point%P
       told = point%T
