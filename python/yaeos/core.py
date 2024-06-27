9f564223efa0d45abb16f0e63946cfa90c3d0d8b
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-06-27 16:05:48 -0300

    modular mixing

--- python/yaeos/core.py
+++ python/yaeos/core.py
@@ -9,58 +9,60 @@ from yaeos import yaeos_c as yaeos
 
 class ArModel(ABC):
 
-    def fugacity(self, n, v, t):
-        return yaeos.fug_vt(self.id, n, v, t)
+    def fugacity(self, n, v, t, dt=None, dp=None, dn=None):
+
+        nc = len(n)
+
+        if dt:
+            dt = np.empty(nc, order="F")
+        if dp:
+            dp = np.empty(nc, order="F")
+        if dn:
+            dn = np.empty((nc, nc), order="F")
+
+        res = yaeos.fug_vt(
+            self.id, 
+            n, v, t, 
+            dlnphidt=dt, dlnphidp=dp, dlnphidn=dn
+            )
+        res = {
+            "ln_phi": res, "dt": dt, "dp": dp, "dn": dn
+        }
+        return res
 
     def __del__(self):
         yaeos.make_available_ar_models_list(self.id)
 
 
-class PengRobinson76(ArModel):
-    name = "PengRobinson76"
+class CubicMixRule(ABC):
+    @abstractmethod
+    def set_mixrule(ar_model_id):
+        raise NotImplementedError
 
-    def __init__(self, tc, pc, w, kij=None, lij=None):
-        self.id = np.array(id(self), np.int64)
 
-        n = len(tc)
+class CubicEoS(ArModel):
+    def __init__(self, tc, pc, w):
+        nc = len(tc)
+        self.nc = nc
         self.tc = tc
         self.pc = pc
         self.w = w
 
-        if kij is None:
-            self.kij = np.zeros((n, n))
-        else:
-            self.kij = kij
 
-        if lij is None:
-            self.lij = np.zeros((n, n))
-        else:
-            self.lij = lij
+class QMR(ABC):
 
-        self.id = yaeos.pr76(self.tc, self.pc, self.w, self.kij, self.lij)
+    def __init__(self, kij, lij):
+        self.kij = kij
+        self.lij = lij
+
+    def set_mixrule(self, ar_model_id):
+        yaeos.set_qmr(ar_model_id, self.kij, self.lij)
 
 
-def run_models():
-    tc = np.array([190, 310])
-    pc = np.array([14, 30])
-    w = np.array([0.001, 0.03])
-
-    kij = np.zeros((2, 2))
-    kij[0, 1] = 0.1
-    kij[1, 0] = 0.1
-
-    lij = kij / 2
-
-    m1 = PengRobinson76(tc, pc, w, kij, lij)
-    m2 = PengRobinson76(tc / 2, pc, w, kij, lij)
-
-    n = [0.3, 0.7]
-    v = 1
-    t = 150
-
-    print(m1.fugacity(n, v, t))
-    print(m2.fugacity(n, v, t))
-    print(m1.fugacity(n, v, t))
-
-
-# run_models()
+class PengRobinson76(CubicEoS):
+    name = "PengRobinson76"
+    
+    def __init__(self, tc, pc, w, mixrule: CubicMixRule):
+        super(PengRobinson76, self).__init__(tc, pc, w)
+        self.id = yaeos.pr76(self.tc, self.pc, self.w)
+        mixrule.set_mixrule(self.id)
--- python/yaeos_c.f90
+++ python/yaeos_c.f90
@@ -4,50 +4,54 @@ module yaeos_c
    !! on using it on Python.
    !!
    !! # Description
-   !! The interface holds two lists of models (one for `ArModels` and another 
+   !! The interface holds two lists of models (one for `ArModels` and another
    !! for`GeModels`), and two lists of logicals that represent wich models are
    !! in those lists are allocated.
    !!
    !! When a model is instanciated/allocated, it is stored in the singleton
    !! object `x_model` and right after that the `extend_x_models_list` procedure
    !! is called. This procedure searches for the first `free_x_models` id and
-   !! allocates the singleton model there, returning the `id` of the procedure. 
+   !! allocates the singleton model there, returning the `id` of the procedure.
    use iso_c_binding, only: c_double, c_int, c_int64_t
    use yaeos, only: &
-                    ! Generic Models
-                    & ArModel, GeModel, &
-                    
-                    ! Cubic Models
-                    & CubicEoS, &
-                    & SoaveRedlichKwong, PengRobinson76, PengRobinson78, &
-                    & CubicMixRule, QMR, MHV, &
+   ! Generic Models
+   & ArModel, GeModel, &
 
-                    ! Ge Models
-                    & fNRTL => NRTL, &
-                    
-                    ! Thermodynamic properties
-                    & fugacity_vt
+   ! Cubic Models
+   & CubicEoS, &
+   & SoaveRedlichKwong, PengRobinson76, PengRobinson78, &
+   & CubicMixRule, QMR, MHV, &
+
+   ! Ge Models
+   & fNRTL => NRTL, &
+
+   ! Thermodynamic properties
+   & fugacity_vt
    implicit none
 
    private
 
-   public :: pr76, srk, fug_vt, make_available_ar_models_list
+   public :: pr76
+   public :: srk
+   public :: fug_vt
+   public :: make_available_ar_models_list
+   public :: set_mhv, set_qmr
 
    type :: ArModelContainer
-      !! Container type for ArModels 
+      !! Container type for ArModels
       class(ArModel), allocatable :: model
-   end type
-   
+   end type ArModelContainer
+
    type :: GeModelContainer
-      !! Container type for GeModels 
+      !! Container type for GeModels
       class(GeModel), allocatable :: model
-   end type
+   end type GeModelContainer
 
    class(ArModel), allocatable :: ar_model !! Singleton to hold temporal ArModels
    class(GeModel), allocatable :: ge_model !! Singleton to hold temporal GeModels
 
    ! Containers of models
-   integer, parameter :: max_models  = 100000
+   integer, parameter :: max_models  = 1000000
    logical :: free_ar_model(max_models) = .true.
    logical :: free_ge_model(max_models) = .true.
 
@@ -58,7 +62,7 @@ module yaeos_c
 contains
 
    subroutine extend_ar_models_list(id)
-      !! Find the first available model container and allocate the model 
+      !! Find the first available model container and allocate the model
       !! there. Then return the found id.
       integer(c_int), intent(out) :: id
       integer :: i
@@ -74,28 +78,68 @@ contains
          end if
       end do
       if (id == max_models) error stop 1
-   end subroutine
-   
+   end subroutine extend_ar_models_list
+
    subroutine make_available_ar_models_list(id)
       !! Make the ArModel id available for allocation
       integer(c_int), intent(in) :: id
       free_ar_model(id) = .true.
-   end subroutine
+   end subroutine make_available_ar_models_list
 
-   subroutine pr76(tc, pc, w, kij, lij, id) bind(C, name="PR76")
-      real(c_double), intent(in) :: tc(:), pc(:), w(:), kij(:, :), lij(:, :)
-      integer(c_int), intent(out) :: id
-      
-      ar_model = PengRobinson76(tc, pc, w, kij, lij)
-      call extend_ar_models_list(id)
-   end subroutine
+   subroutine set_mhv(ar_id, ge_id, q)
+      integer(c_int), intent(in) :: ar_id
+      integer(c_int), intent(in) :: ge_id
+      real(c_double), intent(in) :: q
+
+      type(MHV) :: mixrule
+
+      ar_model = ar_models(ar_id)%model
+      ge_model = ge_models(ge_id)%model
+
+      select type(ar_model)
+       class is(CubicEoS)
+         mixrule = MHV(ge=ge_model, b=ar_model%b, q=q)
+         deallocate(ar_model%mixrule)
+         ar_model%mixrule = mixrule
+      end select
+
+      call move_alloc(ar_model, ar_models(ar_id)%model)
+   end subroutine set_mhv
    
+   subroutine set_qmr(ar_id, kij, lij)
+      use yaeos, only: QMR
+      integer(c_int), intent(in) :: ar_id
+      real(c_double) :: kij(:, :)
+      real(c_double) :: lij(:, :)
+
+      type(QMR) :: mixrule
+
+      ar_model = ar_models(ar_id)%model
+
+      select type(ar_model)
+       class is(CubicEoS)
+         mixrule = QMR(k=kij, l=lij)
+         deallocate(ar_model%mixrule)
+         ar_model%mixrule = mixrule
+      end select
+
+      call move_alloc(ar_model, ar_models(ar_id)%model)
+   end subroutine set_qmr
+
+   subroutine pr76(tc, pc, w, id) bind(C, name="PR76")
+      real(c_double), intent(in) :: tc(:), pc(:), w(:)
+      integer(c_int), intent(out) :: id
+
+      ar_model = PengRobinson76(tc, pc, w)
+      call extend_ar_models_list(id)
+   end subroutine pr76
+
    subroutine srk(tc, pc, w, kij, lij, id)
       real(c_double), intent(in) :: tc(:), pc(:), w(:), kij(:, :), lij(:, :)
       integer(c_int), intent(out) :: id
       ar_model = SoaveRedlichKwong(tc, pc, w, kij, lij)
       call extend_ar_models_list(id)
-   end subroutine
+   end subroutine srk
 
    subroutine fug_vt(id, n, v, t, lnfug, dlnphidp, dlnphidt, dlnphidn)
       integer(c_int), intent(in) :: id
@@ -109,6 +153,6 @@ contains
       call fugacity_vt(&
          ar_models(id)%model, &
          n, V, T, P, lnfug, dlnPhidP, dlnphidT, dlnPhidn &
-      )
-   end subroutine
-end module
+         )
+   end subroutine fug_vt
+end module yaeos_c
