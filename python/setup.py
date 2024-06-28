d39a67b9f4c6c4284ca80a8df400d8c17896ca6e
fedebenelli <fedebenelli@outlook.com>
2024-06-28 00:55:54 -0300

    blacked

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,7 @@ import shutil
 from setuptools.command.install import install
 from pathlib import Path
 
+
 class BuildFortran(Command):
     description = "Compile Fortran library with fpm and f2py"
     user_options = []
@@ -14,32 +15,40 @@ class BuildFortran(Command):
     def finalize_options(self):
         pass
 
-    def run(self):      
+    def run(self):
         build_dir = Path("..") / "build" / "python"
         link_dir = build_dir.absolute() / "lib"
         incl_dir = build_dir.absolute() / "include"
         this_dir = Path(".")
 
-        subprocess.check_call([
-           "fpm", "install", "--profile", "release", "--prefix", build_dir
-        ])
+        subprocess.check_call(
+            ["fpm", "install", "--profile", "release", "--prefix", build_dir]
+        )
 
-        subprocess.check_call([
-           "f2py",
-           "-m", "yaeos_compiled",
-           f"-L{link_dir}",
-           f"-I{incl_dir}",
-           "-c", "yaeos/fortran_wrap/yaeos_c.f90", "-lyaeos",
-           "--backend", "meson",
-        ])
+        subprocess.check_call(
+            [
+                "f2py",
+                "-m",
+                "yaeos_compiled",
+                f"-L{link_dir}",
+                f"-I{incl_dir}",
+                "-c",
+                "yaeos/fortran_wrap/yaeos_c.f90",
+                "-lyaeos",
+                "--backend",
+                "meson",
+            ]
+        )
 
         for file in this_dir.glob("yaeos_compiled.*"):
             shutil.move(file, this_dir / "yaeos/fortran_wrap")
+
 
 class CustomInstall(install):
     def run(self):
         self.run_command("build_fortran")
         install.run(self)
+
 
 setup(
     name="yaeos",
@@ -49,8 +58,5 @@ setup(
         "build_fortran": BuildFortran,
         "install": CustomInstall,
     },
-    install_requires=[
-       "numpy",
-       "fpm"
-    ],
+    install_requires=["numpy", "fpm"],
 )
