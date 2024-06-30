f5be76a07ee9431a0cf9019f53fb8629b1fd090e
fedebenelli <fedebenelli@outlook.com>
2024-06-29 23:10:39 -0300

    det

--- app/phase_diagram.f90
+++ app/phase_diagram.f90
@@ -4,50 +4,35 @@ program phase_diagram
    use yaeos__phase_equilibria_auxiliar, only: k_wilson
    implicit none
 
+   integer, parameter :: nc=2
    class(ArModel), allocatable :: model
    type(EquilibriaState) :: init
    type(PTEnvel2) :: envelope
 
-   integer, parameter :: nc=2
-   integer :: i
-   real(pr) :: n(nc), tc(nc), pc(nc), w(nc), kij(nc, nc), lij(nc, nc), T
+   real(pr) :: n(nc), tc(nc), pc(nc), w(nc), T
    type(Substance) :: sus(nc)
 
    forsus_dir = "build/dependencies/forsus/" // forsus_default_dir
+
    sus(1) = Substance("methane")
    sus(2) = Substance("n-hexane")
+   call get_critical_constants(sus, tc, pc, w)
+
+   model = PengRobinson76(tc, pc, w)
 
    n = [0.9_pr, 0.1_pr]       ! Composition
-   tc = sus%critical%critical_temperature%value
-   pc = sus%critical%critical_pressure%value/1e5
-   w = sus%critical%acentric_factor%value
-
-   kij = 0
-   kij(1, 2) = 0.0
-   kij(2, 1) = kij(1, 2)
-
-   lij =  0
-
-   model = PengRobinson76(tc, pc, w, kij, lij)
-
-   ! Calculate a dew point
    T = 150
-   do i=0, 6
-      kij(1, 2) = 0._pr + real(i,pr)/10
-      kij(2, 1) = kij(1, 2)
-      lij = kij/10
+   init = saturation_temperature(model, n, P=1._pr, kind="dew", t0=150._pr)
+   envelope = pt_envelope_2ph(model, n, init, delta_0=0.01_pr, points=1000)
+   write(*, *) envelope
 
-      model = PengRobinson78(tc, pc, w, kij, lij)
+contains
 
-      init = saturation_temperature(model, n, P=1._pr, kind="dew", t0=150._pr)
-      init%x = 1/k_wilson(model, init%T, init%P) * init%y
-      envelope = pt_envelope_2ph(model, n, init, delta_0=0.01_pr, points=1000)
-      write(1, *) envelope
-
-      init = saturation_pressure(model, n, T=150._pr, kind="bubble", p0=40._pr)
-      envelope = pt_envelope_2ph(model, n, init, points=1000)
-      write(1, *) envelope
-
-   end do
-  
+   subroutine get_critical_constants(subs, tc, pc, w)
+      type(Substance) :: subs(:)
+      real(pr) :: tc(:), pc(:), w(:)
+      tc = sus%critical%critical_temperature%value
+      pc = sus%critical%critical_pressure%value/1e5
+      w = sus%critical%acentric_factor%value
+   end subroutine
 end program phase_diagram
