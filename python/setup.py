1917157ba8c8cf1574f825de6e80f5cddaa3bb47
fedebenelli <fedebenelli@outlook.com>
2024-06-29 21:36:34 -0300

    cleaning

--- python/setup.py
+++ python/setup.py
@@ -5,9 +5,54 @@ from setuptools.command.install import install
 from pathlib import Path
 import sysconfig
 
+
 # =============================================================================
 # Building
 # =============================================================================
+def build_library():
+    build_dir = Path("..") / "build" / "python"
+    link_dir = build_dir.absolute() / "lib"
+    incl_dir = build_dir.absolute() / "include"
+    this_dir = Path(".")
+
+    subprocess.check_call(
+        [
+            "fpm",
+            "install",
+            "--profile",
+            "release",
+            "--flags",
+            "-fPIC",
+            "--c-flags",
+            "-fPIC",
+            "--prefix",
+            build_dir,
+        ]
+    )
+
+    subprocess.check_call(
+        [
+            "f2py",
+            "-m",
+            "yaeos_compiled",
+            f"-L{link_dir}",
+            f"-I{incl_dir}",
+            "-c",
+            "yaeos/fortran_wrap/yaeos_c.f90",
+            "-lyaeos",
+            "--backend",
+            "meson",
+        ]
+    )
+
+    site_packages_dir = Path(sysconfig.get_path("purelib"))
+
+    for file in this_dir.glob("yaeos_compiled.*"):
+        target_dir = site_packages_dir / "yaeos" / "compiled_module"
+        target_dir.mkdir(parents=True, exist_ok=True)
+        shutil.move(file, target_dir)
+
+
 class BuildFortran(Command):
     description = "Compile Fortran library with fpm and f2py"
     user_options = []
@@ -19,41 +64,8 @@ class BuildFortran(Command):
         pass
 
     def run(self):
-        build_dir = Path("..") / "build" / "python"
-        link_dir = build_dir.absolute() / "lib"
-        incl_dir = build_dir.absolute() / "include"
-        this_dir = Path(".")
+        build_library()
 
-        subprocess.check_call(
-            [
-                "fpm", "install",
-                "--profile", "release",
-                "--flags", "-fPIC",
-                "--c-flags", "-fPIC",
-                "--prefix", build_dir]
-        )
-
-        subprocess.check_call(
-            [
-                "f2py",
-                "-m",
-                "yaeos_compiled",
-                f"-L{link_dir}",
-                f"-I{incl_dir}",
-                "-c",
-                "yaeos/fortran_wrap/yaeos_c.f90",
-                "-lyaeos",
-                "--backend",
-                "meson",
-            ]
-        )
-
-        site_packages_dir = Path(sysconfig.get_path("purelib"))
-
-        for file in this_dir.glob("yaeos_compiled.*"):
-            target_dir = site_packages_dir / "yaeos" / "compiled_module"
-            target_dir.mkdir(parents=True, exist_ok=True)
-            shutil.move(file, target_dir)
 
 # =============================================================================
 # Building for developers (editable installation)
@@ -73,34 +85,7 @@ class BuildFortranEditable(Command):
         pass
 
     def run(self):
-        build_dir = Path("..") / "build" / "python"
-        link_dir = build_dir.absolute() / "lib"
-        incl_dir = build_dir.absolute() / "include"
-        this_dir = Path(".")
-
-        subprocess.check_call(
-            ["fpm", "install", "--profile", "release", "--prefix", build_dir]
-        )
-
-        subprocess.check_call(
-            [
-                "f2py",
-                "-m",
-                "yaeos_compiled",
-                f"-L{link_dir}",
-                f"-I{incl_dir}",
-                "-c",
-                "yaeos/fortran_wrap/yaeos_c.f90",
-                "-lyaeos",
-                "--backend",
-                "meson",
-            ]
-        )
-
-        for file in this_dir.glob("yaeos_compiled.*"):
-            target_dir = Path(".") / "yaeos" / "compiled_module"
-            target_dir.mkdir(parents=True, exist_ok=True)
-            shutil.move(file, target_dir)
+        build_library()
 
 
 class CustomInstall(install):
@@ -126,10 +111,6 @@ setup(
     },
     include_package_data=True,
     exclude_package_data={
-        "yaeos": [
-            "__pycache__",
-            "*.f90",
-            "*.egg-info"
-        ],
-    }
+        "yaeos": ["__pycache__", "*.f90", "*.egg-info"],
+    },
 )
