2787a37adb7fe4bf8b08aa4f31c874c752e584fd
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-04 10:50:10 -0300

    prettier?

--- app/phase_diagram.f90
+++ app/phase_diagram.f90
@@ -1,38 +1,58 @@
 program phase_diagram
+   !! Program for calculation of phase diagrams. 
    use forsus, only: Substance, forsus_dir, forsus_default_dir
-   use yaeos
+   use yaeos, only: pr, &
+      SoaveRedlichKwong, PengRobinson76, PengRobinson78, RKPR, &
+      EquilibriaState, ArModel, PTEnvel2, &
+      pt_envelope_2ph, saturation_pressure, saturation_temperature
    use yaeos__phase_equilibria_auxiliar, only: k_wilson
    implicit none
 
-   integer, parameter :: nc=2
-   class(ArModel), allocatable :: model
-   type(EquilibriaState) :: init
-   type(PTEnvel2) :: envelope
+   ! ===========================================================================
+   ! Variables definition
+   ! ---------------------------------------------------------------------------
+   integer, parameter :: nc=2            
+   class(ArModel), allocatable :: model ! Thermodynamic model to be used
+   type(EquilibriaState) :: sat_point   ! Init
+   type(PTEnvel2) :: envelope           ! PT Phase envelope
+   real(pr) :: tc(nc), pc(nc), w(nc)    ! Component's critical constants
+   real(pr) :: n(nc)                    ! Termodynamic variables
+   type(Substance) :: sus(nc)           ! Substances to use
+   ! ===========================================================================
 
-   real(pr) :: n(nc), tc(nc), pc(nc), w(nc), T
-   type(Substance) :: sus(nc)
-
+   ! forsus database directory
    forsus_dir = "build/dependencies/forsus/" // forsus_default_dir
 
+   ! Find the selected substances on the database and extract their
+   ! critical constants
    sus(1) = Substance("methane")
    sus(2) = Substance("n-hexane")
    call get_critical_constants(sus, tc, pc, w)
 
+   ! Model definition
    model = PengRobinson76(tc, pc, w)
 
-   n = [0.9_pr, 0.1_pr]       ! Composition
-   T = 150
-   init = saturation_temperature(model, n, P=1._pr, kind="dew", t0=150._pr)
-   envelope = pt_envelope_2ph(model, n, init, delta_0=0.01_pr, points=1000)
+   ! Composition vector
+   n = [0.9_pr, 0.1_pr]
+   
+   ! Calculate a dew point at low pressure to later 
+   ! initialize the phase envelope
+   sat_point = saturation_temperature(model, n, P=1._pr, kind="dew", t0=150._pr)
+
+   ! Calculate phase envelope
+   envelope = pt_envelope_2ph(model, n, sat_point)
+
+   ! Write the phase envelope to screen
    write(*, *) envelope
 
 contains
 
-   subroutine get_critical_constants(subs, tc, pc, w)
+   subroutine get_critical_constants(subs, tc_in, pc_in, w_in)
       type(Substance) :: subs(:)
-      real(pr) :: tc(:), pc(:), w(:)
-      tc = sus%critical%critical_temperature%value
-      pc = sus%critical%critical_pressure%value/1e5
-      w = sus%critical%acentric_factor%value
+      real(pr), intent(out) :: tc_in(:), pc_in(:), w_in(:)
+
+      tc_in = subs%critical%critical_temperature%value
+      pc_in = subs%critical%critical_pressure%value/1e5
+      w_in = subs%critical%acentric_factor%value
    end subroutine
 end program phase_diagram
