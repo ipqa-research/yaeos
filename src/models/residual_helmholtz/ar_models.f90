d1229cbf8dd8d066b08954e1621e3b96d40cf3d3
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-15 13:45:00 -0300

    generic volume solver for ArModel

--- src/models/residual_helmholtz/ar_models.f90
+++ src/models/residual_helmholtz/ar_models.f90
@@ -30,7 +30,7 @@ module yaeos__models_ar
       !! Method to calculate residual helmholtz energy and derivatives.
       procedure(abs_volume_initializer), deferred :: get_v0
       !! Volume initializer
-   end type
+   end type ArModel
 
    abstract interface
       subroutine abs_residual_helmholtz(&
@@ -67,7 +67,7 @@ module yaeos__models_ar
          real(pr), optional, intent(out) :: ArVn(size(n)) !! \(\frac{d^2Ar}{dVn_i}\)
          real(pr), optional, intent(out) :: ArTn(size(n)) !! \(\frac{d^2Ar}{dTn_i}\)
          real(pr), optional, intent(out) :: Arn2(size(n), size(n))!! \(\frac{d^2Ar}{dn_{ij}}\)
-      end subroutine
+      end subroutine abs_residual_helmholtz
 
       function abs_volume_initializer(self, n, p, t)
          !! Initialization of volume.
@@ -77,6 +77,68 @@ module yaeos__models_ar
          real(pr), intent(in) :: p !! Pressure [bar]
          real(pr), intent(in) :: t !! Temperature [K]
          real(pr) :: abs_volume_initializer !! Initial volume [L]
-      end function
+      end function abs_volume_initializer
    end interface
-end module
+
+contains
+
+   subroutine volume(eos, n, T, P, V, root_type)
+      !! Generic volume solver
+      use yaeos__constants, only: pr, R
+      use yaeos__math, only: newton
+      class(ArModel), intent(in) :: eos
+      real(pr), intent(in) :: n(:)
+      real(pr), intent(in) :: P, T
+      real(pr), intent(out) :: V
+      character(len=*), intent(in) :: root_type
+
+      integer :: max_iters=30
+      real(pr) :: tol=1e-7
+
+      real(pr) :: totnRT, GrL, GrV, Gr
+      real(pr) :: Vliq, Vvap
+
+      GrL = HUGE(GrL)
+      GrV = HUGE(GrV)
+
+      totnRT = sum(n) * R * T
+      select case(root_type)
+       case("liquid")
+         Vliq = eos%get_v0(n, P, T)! *1.001_pr
+         call newton(foo, Vliq, tol=tol, max_iters=max_iters)
+         GrL = Gr
+       case("vapor")
+         Vvap = R * T / P
+         call newton(foo, Vvap, tol=tol, max_iters=max_iters)
+         GrV = Gr
+       case("stable")
+         Vliq = eos%get_v0(n, P, T)*1.00001_pr
+         call newton(foo, Vliq, tol=tol, max_iters=max_iters)
+         GrL = Gr
+         
+         Vvap = R * T / P
+         call newton(foo, Vvap, tol=tol, max_iters=max_iters)
+         GrV = Gr
+      end select
+
+      if (GrL < GrV) then
+         V = Vliq
+      else
+         V = Vvap
+      end if
+
+   contains
+      subroutine foo(x, f, df)
+         real(pr), intent(in) :: x
+         real(pr), intent(out) :: f, df
+         real(pr) :: Ar, ArV, ArV2, Pcalc, dPcalcdV, Vin
+         Vin = x
+         call eos%residual_helmholtz(n, Vin, T, Ar=Ar, ArV=ArV, ArV2=ArV2)
+         Pcalc = totnRT / Vin - ArV
+         dPcalcdV = -totnRT / Vin**2 - ArV2
+         f = Pcalc - p
+         df = dPcalcdV
+         Gr = Ar + P * Vin - totnRT - totnRT * log(P*Vin/(R*T))
+      end subroutine foo
+   end subroutine volume
+end module yaeos__models_ar
