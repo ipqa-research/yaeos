f318fd073e614073e42d091e093e775f1c010241
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-16 15:30:49 -0300

    isotherm

--- app/isotherm.f90
+++ app/isotherm.f90
@@ -11,32 +11,39 @@ program main
     real(pr), dimension(nc) :: n, tc, pc, w
     real(pr), dimension(nc, nc) :: kij, lij
 
+    type(timer) :: tim
+
     type(Substance) :: sus(2)
 
     real(pr) :: v, T, P
     real(pr) :: lnphip(nc)
 
+    integer :: i, nevals=100000
+
     forsus_dir = "build/dependencies/forsus/data/json"
     sus(1) = Substance("propane", only=["critical"])
     sus(2) = Substance("n-butane", only=["critical"])
 
-    n = [0.7, 0.3]
+    n = [0.99, 0.01]
     tc = sus%critical%critical_temperature%value
     pc = sus%critical%critical_pressure%value/1e5
     w = sus%critical%acentric_factor%value
 
     eos = SoaveRedlichKwong(tc, pc, w)
     T = 350
-    P = 10
+    P = 500
 
-    call eos%volume(n, P=P,T=T, root_type="stable", V=V)
-    print *, V, P
-    call volume_michelsen(eos, n, P=P,T=T, root_type="stable", V=V)
-    print *, V, P
+    do i=1,10000
+        P = real(i, pr)/10
+        call eos%volume(n, P, T, V, "stable")
+        print *, V, P
+    end do
 
+    print "(/)"
 
-    call eos%pressure(n, V, T, P)
-    print *, T, V, P
-    call eos%lnphi_tp(n, T, P, V=V, root_type="vapor", lnphip=lnphip)
-    print *, T, P, V, lnphip
+    do i=1,1000
+        P = real(i, pr)/10
+        call volume_michelsen(eos, n, P, T, V, "stable")
+        print *, V, P
+    end do
 end program main
