bf4d3dd04d4bdf8c7e0f1269010cc3bf924c32db
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-18 11:34:31 -0300

    example of calculating saturation points of pure components

--- example/extra/pure_psat.f90
+++ example/extra/pure_psat.f90
@@ -1,54 +1,70 @@
 !! Program to calculate the vapor pressure of pure components
 !!
+
+module pure_psat
+   !! Module used to calculate the saturation pressure of pure components at
+   !! a given temperature.
+   use yaeos
+contains
+   real(pr) function Psat(eos, ncomp, T)
+      use yaeos__math, only: solve_system
+      class(ArModel), intent(in) :: eos
+      integer, intent(in) :: ncomp
+      real(pr), intent(in) :: T
+
+      real(pr) :: P1, P2
+      real(pr) :: f1, f2
+
+      real(pr) :: n(size(eos))
+
+      n = 0
+      n(ncomp) = 1
+
+      P1 = 0.5
+      P2 = 1
+
+      do while(abs(diff(P2)) > 1e-10)
+         f1 = diff(P1)
+         f2 = diff(P2)
+         Psat = (P1 * f2 - P2 * f1)/(f2 - f1)
+         P1 = P2
+         P2 = Psat
+      end do
+
+   contains
+      real(pr) function diff(P)
+         real(pr), intent(in) :: P
+         real(pr) :: V_l, V_v
+         real(pr) :: phi_v(size(eos)), phi_l(size(eos))
+         call eos%lnphi_pt(n, P, T, V=V_v, lnPhi=phi_v, root_type="vapor")
+         call eos%lnphi_pt(n, P, T, V=V_l, lnPhi=phi_l, root_type="liquid")
+         diff = phi_v(1) - phi_l(1)
+      end function
+   end function Psat
+end module
+
 program main
    use yaeos
    use forsus, only: Substance, forsus_default_dir, forsus_dir
+   use pure_psat, only: Psat
    implicit none
    integer, parameter :: nc=2
    type(CubicEoS) :: eos
    type(Substance) :: sus(nc)
-   real(pr) :: n(nc), T, x, f, df
-   integer :: i
+   real(pr) :: n(nc), T,  f
+   integer :: i, j
 
    forsus_dir = "build/dependencies/forsus/" // forsus_default_dir
    sus(1) = Substance("water")
    sus(2) = Substance("ethanol")
-
    eos = SoaveRedlichKwong(&
       sus%critical%critical_temperature%value, &
       sus%critical%critical_pressure%value/1e5,&
       sus%critical%acentric_factor%value &
       )
-
-   n = [1, 0]
-
    T = 273.15_pr + 50
-
-   do i=273+90, nint(sus(1)%critical%critical_temperature%value)
+   do i=273+90, nint(maxval(sus%critical%critical_temperature%value))
       T = real(i,pr)
-      call foo(T, f)
-      print *, T, F
+      print *, T, (Psat(eos, j, T), j=1,nc)
    end do
-
-contains
-
-   subroutine foo(x, f)
-      real(pr), intent(in) :: x
-      real(pr), intent(out) :: f
-      real(pr) :: phi_v(nc), phi_l(nc), dphidV_v, dphidV_l, Ar, P
-      real(pr) :: dlnphi_dt_v(nc), dlnphi_dt_l(nc)
-      real(pr) :: V_l, V_v
-      real(pr) :: diff
-
-      P = 1
-      diff = 1
-      do while(abs(diff) > 1e-8_pr .and. .not. isnan(diff))
-         call eos%lnphi_pt(n, P, x, V=V_v, lnPhi=phi_v, dlnPhidt=dlnphi_dt_v, root_type="vapor")
-         call eos%lnphi_pt(n, P, x, V=V_l, lnPhi=phi_l, dlnPhidt=dlnphi_dt_l, root_type="liquid")
-         diff = exp(phi_v(1)) - exp(phi_l(1))
-         P = P + sign(diff, 0.1_pr)
-      end do
-
-      f = P
-   end subroutine
 end program main
