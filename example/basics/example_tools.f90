555ed3101bca0ebf5510776836aec4c9151c9249
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-19 14:16:41 -0300

    move some examples

--- example/basics/example_tools.f90
+++ example/basics/example_tools.f90
@@ -1,4 +1,4 @@
-! Here there are some functions defined to simplify some examples
+!! Here there are some functions defined to simplify some examples
 module yaeos__example_tools
    use yaeos
 
@@ -9,13 +9,10 @@ contains
       integer, parameter :: nc = 2
       real(pr) :: tc(nc), pc(nc), w(nc), kij(nc,nc)
 
-      integer :: i
-
       ! Methane/ Butane mixture
       tc = [190.564, 425.12]     ! Critical temperatures
       pc = [45.99, 37.96]        ! Critical pressures
       w = [0.0115478, 0.200164]  ! Acentric factors
-
       model = PengRobinson76(tc, pc, w)
    end function methane_butane_pr76
 end module yaeos__example_tools
