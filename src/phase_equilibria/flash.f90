c0912912a38bac54dba2339e2fb3a2d7ef83b983
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-07-24 16:10:02 -0300

    Moved Pressure equality outside, since it can be a useful solver for other kind of applications

--- src/phase_equilibria/flash.f90
+++ src/phase_equilibria/flash.f90
@@ -4,6 +4,7 @@ module yaeos__equilibria_flash
    use yaeos__equilibria_equilibria_state, only: EquilibriumState
    use yaeos__phase_equilibria_rachford_rice, only: betato01, betalimits, rachford_rice, solve_rr
    use yaeos__phase_equilibria_auxiliar, only: k_wilson
+   use yaeos__solvers_pressure_equality, only: pressure_equality_V_beta_xy
    implicit none
 
 contains
@@ -108,7 +109,7 @@ contains
          select case (spec)
           case("TV")
             ! find Vy,Vx (vV and vL) from V balance and P equality equations
-            call tv_loop_solve_pressures(model, T, V, beta, x, y, Vx, Vy, P)
+            call pressure_equality_V_beta_xy(model, T, V, beta, x, y, Vx, Vy, P)
             call model%lnphi_pt(y, P, T, V=Vy, root_type="stable", lnPhi=lnfug_y)
             call model%lnphi_pt(x, P, T, V=Vx, root_type="liquid", lnPhi=lnfug_x)
           case("TP")
@@ -181,83 +182,4 @@ contains
       flash%vy = vy
       flash%beta = beta
    end function flash
-
-   subroutine tv_loop_solve_pressures(model, T, V, beta, x, y, vx, vy, P)
-      !! Solve pressure equality between two phases at a given temperature,
-      !! total volume, vapor molar fractions and compositions.
-      use iso_fortran_env, only: error_unit
-
-      class(ArModel), intent(in) :: model
-      real(pr), intent(in) :: T !! Temperature [K]
-      real(pr), intent(in) :: V !! Total volume [L/mol]
-      real(pr), intent(in) :: beta !! Molar fraction of light-phase
-      real(pr), intent(in) :: x(:) !! Molar fractions of heavy-phase
-      real(pr), intent(in) :: y(:) !! Molar fractions of light-phase
-      real(pr), intent(in out) :: Vx !! Heavy-phase molar volume [L/mol]
-      real(pr), intent(in out) :: Vy !! Light-Phase molar volume [L/mol]
-      real(pr), intent(out) :: P !! Pressure [bar]
-
-      real(pr) :: Bx !! Liquid phase covolume
-      real(pr) :: dVydVx !! Derivative of Vy wrt Vx
-
-      ! Pressure equality newton functions
-      real(pr) :: h !! Pressure equality
-      real(pr) :: dh  !! dh/
-      real(pr) :: stepv
-
-      real(pr) :: dPxdV, dPydV
-      real(pr) :: Px, Py
-
-      integer :: its
-
-      dVydVx = -(1 - beta)/beta
-      Bx = model%get_v0(x, 0.1_pr, T)
-
-      ! First evaluation will be with Vx = 1.5*Bx
-      if (Vx < Bx) Vx = 1.625_pr*Bx
-
-      call model%pressure(x, Vx, T, Px, dpdv=dPxdV)
-
-      do while (Px < 0 .or. dPxdV >= 0)
-         Vx = Vx - 0.2*(Vx - Bx)
-         call model%pressure(x, Vx, T, Px, dpdv=dPxdV)
-      end do
-
-      Vy = (V - (1 - beta)*Vx)/beta
-
-      h = 1.0
-      its = 0
-      do while (abs(h) > 1.d-4)
-         ! Newton for solving P equality, with Vx as independent variable
-         its = its + 1
-
-         call model%pressure(x, Vx, T, Px, dpdv=dPxdV)
-         call model%pressure(y, Vy, T, Py, dpdv=dPydV)
-
-         h = Py - Px
-         dh = -dPydV * dVydVx - dPxdV
-         stepv = -h/dh
-
-         if (its >= 10) stepv = stepv/2
-
-         Vx = Vx + stepv
-
-         do while (Vx < 1.001*Bx)
-            stepv = stepv/2
-            Vx = Vx - stepv
-         end do
-
-         Vy = (v - (1 - beta)*Vx)/beta
-
-         if (its >= 100) then
-            write (error_unit, *) "WARN(FLASH_VT): volume convergence problems", Px, Py
-            P = -1.0
-            return
-         end if
-      end do
-
-      call model%pressure(x, Vx, T, Px)
-      call model%pressure(y, Vy, T, Py)
-      P = (Px + Py) * 0.5_pr
-   end subroutine tv_loop_solve_pressures
 end module yaeos__equilibria_flash
