22c34893009634086ef468b3312656ba3ecb2cad
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2024-08-13 10:54:33 -0300

    no nlopt

--- python/cibuildbuild_linux.sh
+++ python/cibuildbuild_linux.sh
@@ -1,10 +1,10 @@
 yum -y update && yum -y install blas-devel lapack-devel freetype-devel
 
-wget https://github.com/stevengj/nlopt/archive/refs/tags/v2.7.1.tar.gz
-tar xf v2.7.1.tar.gz 
-cd v2.7.1.tar.gz 
-
-mkdir build
-cd build
-cmake ..
-sudo make install
+# wget https://github.com/stevengj/nlopt/archive/refs/tags/v2.7.1.tar.gz
+# tar xf v2.7.1.tar.gz 
+# cd v2.7.1.tar.gz 
+# 
+# mkdir build
+# cd build
+# cmake ..
+# sudo make install
