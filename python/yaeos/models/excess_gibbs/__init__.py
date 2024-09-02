fb1e4270873c53fe358af73d2b675b241b1f230b
Salvador <salvadorebrandolin@gmail.com>
2024-09-02 11:20:43 -0300

    modifications to documentation

--- python/yaeos/models/excess_gibbs/__init__.py
+++ python/yaeos/models/excess_gibbs/__init__.py
@@ -4,6 +4,7 @@ Yaeos Gibbs excess module. This module provides the following submodules:
 
 - excess_gibbs: Excess Gibbs energy models
     - NRTL: non-random two-liquid model
+    - UNIFACVLE: Original UNIFAC VLE model
 """
 
 from .nrtl import NRTL
