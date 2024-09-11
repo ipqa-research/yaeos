a2e33e61b0178f0b81ac40d9617ef2a1e10430af
Salvador <salvadorebrandolin@gmail.com>
2024-09-11 16:36:43 -0300

    keep working at home

--- app/uniquac.f90
+++ app/uniquac.f90
@@ -3,13 +3,11 @@ program cositom
 
    type(UNIQUAC) :: model
 
-   real(pr) :: qs(2), rs(2), T, z(2), Ge, GeT, GeT2
+   real(pr) :: qs(2), rs(2), T, z(2), Ge, GeT, GeT2, Gen(2), Gen2(2,2), GeTn(2)
    real(pr), dimension(2,2) :: A, B, C, D, E
 
-   T = 25.0 + 273.15
-
-   rs = [0.92, 2.1055]
-   qs = [1.4, 1.972]
+   rs = [0.92_pr, 2.1055_pr]
+   qs = [1.4_pr, 1.972_pr]
 
    A = reshape([[0.0_pr, -75.46_pr, 120.20_pr, 0.0_pr]], [2,2])
    B = reshape([[0.0_pr, -0.10062_pr, 0.44835_pr, 0.0_pr]], [2,2])
@@ -23,7 +21,7 @@ program cositom
    T = 298.15_pr
    z = [0.5_pr, 0.5_pr]
 
-   call model%excess_gibbs(z, T, Ge=Ge, GeT=GeT, GeT2=GeT2)
+   call model%excess_gibbs(z, T, Ge=Ge, GeT=GeT, GeT2=GeT2, Gen=Gen, Gen2=Gen2, GeTn=GeTn)
 
    print *, 'Ge = ', Ge * 100
    print *, "Ge_thermo = ", -203914.9332908132_pr
@@ -33,5 +31,8 @@ program cositom
    print *, "=================================================================="
    print *, "GeT2 = ", GeT2 * 100
    print *, "GeT2_thermo = ", 0.11574726032863471_pr
+   print *, "=================================================================="
+   print *, "Gen = ", Gen * 100
+   print *, "Gen_thermo = ", [-101957.4666454066_pr, -101957.4666454066_pr]
 
 end program cositom
