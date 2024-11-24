01eb93519fdf168991cbbab0171c2809a52c7ea1
fedebenelli <fedebenelli@outlook.com>
2024-11-24 11:55:26 -0300

    test print

--- test/test_huron_vidal.f90
+++ test/test_huron_vidal.f90
@@ -24,7 +24,7 @@ program main
 
    model = binary_NRTL_SRK_HV()
 
-   print *, "HURON_VIDAL MIXING RULE"
+   write(*, *) "TESTING: HURON_VIDAL MIXING RULE"
 
    n = [0.2, 0.8]
    T = 150
@@ -39,42 +39,52 @@ program main
 
    call model%mixrule%Dmix(n, T, ai, daidt, daidt2, D, dDdT, dDdT2, dDi, dDidT, dDij)
    
+   if (.not. allclose([D], [test_D], test_tol)) error stop 1
+   if (.not. allclose([dDdT], [test_dDdT], test_tol)) error stop 1
+   if (.not. allclose([dDdT2], [test_dDdT2], test_tol)) error stop 1
+   if (.not. allclose([dDi], [test_dDi], test_tol)) error stop 1
+   if (.not. allclose([dDidT], [test_dDidT], test_tol)) error stop 1
+   if (.not. allclose([dDij], [test_dDij], test_tol)) error stop 1
+
+   ! Below are the numerical derivatives used to generate the test values 
+
+   
    ! print *, "numdiff"
 
    ! print *, "dDdT", dDdT,   (Dhv(n, T+0.01) - Dhv(n, T))/0.01
    ! print *, "dDdT2", dDdT2, (Dhv(n, T+0.01) - 2*Dhv(n, T) + Dhv(n, T-0.01))/(0.01**2)
 
    ! print *, "dDi"
-   do i = 1, nc
-      dn = 0
-      dn(i) = 0.001
-      ! print *, dDi(i), (Dhv(n + dn, T) - Dhv(n - dn, T))/(2 * dn(i))
-   end do
+   ! do i = 1, nc
+   !   dn = 0
+   !   dn(i) = 0.001
+   !   print *, dDi(i), (Dhv(n + dn, T) - Dhv(n - dn, T))/(2 * dn(i))
+   ! end do
 
    ! print *, "dDidT"
-   do i = 1, nc
-      dn = 0
-      dn(i) = 0.005
+   ! do i = 1, nc
+   !    dn = 0
+   !    dn(i) = 0.005
 
-      print *, dDidT(i), (&
-           Dhv(n + dn, T + dn(i)) &
-         - Dhv(n + dn, T - dn(i)) &
-         - Dhv(n - dn, T + dn(i)) &
-         + Dhv(n - dn, T - dn(i)) &
-         )/(4 * dn(i)**2)
-   end do
+   !    print *, dDidT(i), (&
+   !         Dhv(n + dn, T + dn(i)) &
+   !       - Dhv(n + dn, T - dn(i)) &
+   !       - Dhv(n - dn, T + dn(i)) &
+   !       + Dhv(n - dn, T - dn(i)) &
+   !       )/(4 * dn(i)**2)
+   ! end do
 
    ! print *, "dDij"
 
-   dn = 0
-   dn(1) = 1e-5
+   ! dn = 0
+   ! dn(1) = 1e-5
    ! print *, dDij(1, 1), (Dhv(n + dn, T) - 2*Dhv(n, T) + Dhv(n - dn, T))/(dn(1)**2)
 
-   dn = 0
-   dn(2) = 1e-5
+   ! dn = 0
+   ! dn(2) = 1e-5
    ! print *, dDij(2, 2), (Dhv(n + dn, T) - 2*Dhv(n, T) + Dhv(n - dn, T))/(dn(2)**2)
   
-   dx = 1e-6
+   ! dx = 1e-6
    
    ! print *, dDij(1, 2), (&
    !         Dhv(n + [dx, dx], T) &
@@ -84,32 +94,25 @@ program main
    !       )/(4 * dx **2)
   
 
-   if (.not. allclose([D], [test_D], test_tol)) error stop 1
-   if (.not. allclose([dDdT], [test_dDdT], test_tol)) error stop 1
-   if (.not. allclose([dDdT2], [test_dDdT2], test_tol)) error stop 1
-   if (.not. allclose([dDi], [test_dDi], test_tol)) error stop 1
-   if (.not. allclose([dDidT], [test_dDidT], test_tol)) error stop 1
-   if (.not. allclose([dDij], [test_dDij], test_tol)) error stop 1
+   ! contains
 
-contains
+   !real(pr) function Dhv(n, T)
+   !   real(pr), intent(in) :: n(:), T
 
-   real(pr) function Dhv(n, T)
-      real(pr), intent(in) :: n(:), T
+   !   real(pr) :: Tc(size(n)), Tr(size(n))
+   !   real(pr), dimension(size(n)) :: ai, daidt, dadit2
+   !   real(pr) :: D, dDdT, dDdT2, dDi(size(n)), dDidT(size(n)), dDij(size(n), size(n))
+   !   
+   !   Tc = model%components%Tc
+   !   Tr = T/Tc
 
-      real(pr) :: Tc(size(n)), Tr(size(n))
-      real(pr), dimension(size(n)) :: ai, daidt, dadit2
-      real(pr) :: D, dDdT, dDdT2, dDi(size(n)), dDidT(size(n)), dDij(size(n), size(n))
-      
-      Tc = model%components%Tc
-      Tr = T/Tc
+   !   call model%alpha%alpha(Tr, ai, daidt, daidt2)
 
-      call model%alpha%alpha(Tr, ai, daidt, daidt2)
+   !   ai = ai*model%ac
+   !   daidt = daidt*model%ac/Tc
+   !   daidt2 = daidt2*model%ac/Tc**2
 
-      ai = ai*model%ac
-      daidt = daidt*model%ac/Tc
-      daidt2 = daidt2*model%ac/Tc**2
-
-      call model%mixrule%Dmix(n, T, ai, daidt, daidt2, D, dDdT, dDdT2, dDi, dDidT, dDij)
-      Dhv = D
-   end function
+   !   call model%mixrule%Dmix(n, T, ai, daidt, daidt2, D, dDdT, dDdT2, dDi, dDidT, dDij)
+   !   Dhv = D
+   !end function
 end program main
