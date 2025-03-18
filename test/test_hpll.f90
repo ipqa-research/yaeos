c700dbd968c1d19605e18197e9c0e1051400dee9
Federico E. Benelli <federico.benelli@mi.unc.edu.ar>
2025-03-18 14:44:03 -0300

    test

--- test/test_hpll.f90
+++ test/test_hpll.f90
@@ -15,8 +15,8 @@ program main
 
    env = find_hpl(model, z, T0=500._pr, p0=1500._pr, max_points=1000)
 
-  if(abs(env%points(1)%T - 103.69) > 0.01) error stop "HPLL Failed T1"
-  if(abs(env%points(1)%P - 1500) > 0.1) error stop "HPLL Failed T1"
-  if (abs(env%points(size(env%points))%P) > 0.15) error stop "HPLL Failed Pend"
+   if(abs(env%points(1)%T - 103.69) > 0.01) error stop "HPLL Failed T1"
+   if(abs(env%points(1)%P - 1500) > 0.1) error stop "HPLL Failed T1"
+   if (abs(env%points(size(env%points))%P) > 0.15) error stop "HPLL Failed Pend"
 
-end program
+end program main
