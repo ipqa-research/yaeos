d84ff98eccf716b733899cd3989082a6f608bd77
fedebenelli <fedebenelli@outlook.com>
2025-03-25 11:31:35 -0300

    print

--- test/test_implementations/ar_models/cubics/test_pr76.f90
+++ test/test_implementations/ar_models/cubics/test_pr76.f90
@@ -642,6 +642,7 @@ contains
       ! Critical point check
       ! -----------------------------------------------------------------------
       print *, "Critical point: ", envelope%cps(1)%T, envelope%cps(1)%P
+      print *, envelope
       call check(error, abs(envelope%cps(1)%T - 532.9_pr) < 1)
       call check(error, abs(envelope%cps(1)%P - 32.95_pr) < 0.2)
       ! =======================================================================
