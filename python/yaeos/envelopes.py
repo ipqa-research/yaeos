92a214c401a8719bcffcc0cc118694132f1ddb9d
Federico E. Benelli <federico.benelli@unc.edu.ar>
2025-05-19 11:14:14 -0300

    tx

--- python/yaeos/envelopes.py
+++ python/yaeos/envelopes.py
@@ -229,6 +229,7 @@ class PXEnvelope:
             return PXEnvelope(
                 global_composition_0=self.global_composition_0,
                 global_composition_i=self.global_composition_i,
+                temperature=self.temperature,
                 main_phases_compositions=self.main_phases_compositions[key],
                 reference_phase_compositions=self.reference_phase_compositions[
                     key
@@ -259,12 +260,142 @@ class PXEnvelope:
         return PXEnvelope(
             global_composition_0=self.global_composition_0,
             global_composition_i=self.global_composition_i,
+            temperature=self.temperature,
             main_phases_compositions=self.main_phases_compositions * other,
             reference_phase_compositions=self.reference_phase_compositions
             * other,
             main_phases_molar_fractions=self.main_phases_molar_fractions
             * other,
             pressures=self.pressures * other,
+            alphas=self.alphas * other,
+            iterations=self.iterations,
+            specified_variable=self.specified_variable,
+        )
+
+    def __len__(self):
+        return len(self.alphas)
+
+
+class TXEnvelope:
+    """TXEnvelope.
+
+    This class represents a pressure-composition envelope.
+    """
+
+    def __init__(
+        self,
+        pressure,
+        global_composition_0,
+        global_composition_i,
+        main_phases_compositions,
+        reference_phase_compositions,
+        main_phases_molar_fractions,
+        temperatures,
+        alphas,
+        iterations,
+        specified_variable,
+    ):
+
+        msk = ~np.isnan(temperatures)
+        self.temperature = pressure
+        self.number_of_components = len(global_composition_0)
+        self.number_of_phases = main_phases_compositions.shape[1]
+        self.global_composition_0 = global_composition_0
+        self.global_composition_i = global_composition_i
+        self.main_phases_compositions = main_phases_compositions[msk, :, :]
+        self.reference_phase_compositions = reference_phase_compositions[
+            msk, :
+        ]
+        self.main_phases_molar_fractions = main_phases_molar_fractions[msk]
+        self.temperatures = temperatures[msk]
+        self.alphas = alphas[msk]
+        self.iterations = iterations[msk]
+        self.specified_variable = specified_variable[msk]
+
+        df = pd.DataFrame()
+
+        df["alpha"] = self.alphas
+        df["T"] = self.temperatures
+
+        for i in range(self.number_of_components):
+            for j in range(self.number_of_phases):
+                df[f"x_{i+1}^{j+1}"] = self.main_phases_compositions[:, j, i]
+            df[f"w_{i+1}"] = self.reference_phase_compositions[:, i]
+
+        for i in range(self.number_of_phases):
+            df[f"beta^{i+1}"] = self.main_phases_molar_fractions[:, i]
+
+        self.df = df
+
+        idx = []
+        for phase in range(self.number_of_phases):
+            cp_idx = []
+            lnK = np.log(
+                self.reference_phase_compositions
+                / self.main_phases_compositions[:, phase]
+            )
+
+            for i in range(1, len(lnK)):
+                if all(lnK[i, :] * lnK[i - 1, :] < 0):
+                    cp_idx.append(i)
+
+            idx.append(cp_idx)
+
+        self.cp = idx
+
+        # lnKs1 = np.log(c["w"][1:]/c["x_l"][1:, 1])
+        # lnKs0 = np.log(c["w"][1:]/c["x_l"][1:, 0])
+        # for i, lnK in enumerate(np.log(c["w"][1:]/c["x_l"][1:, 1])):
+        #     lnK2 = np.log(c["w"]/c["x_l"][i, 1])
+        #     crit = (lnK * lnK2 < 0).all()
+        #     if crit:
+        #         print("asd")
+
+    def __getitem__(self, key):
+        if "key" in self.__dict__:
+            return self.__dict__["key"]
+        elif isinstance(key, np.ndarray):
+            return TXEnvelope(
+                global_composition_0=self.global_composition_0,
+                global_composition_i=self.global_composition_i,
+                pressure=self.pressure,
+                main_phases_compositions=self.main_phases_compositions[key],
+                reference_phase_compositions=self.reference_phase_compositions[
+                    key
+                ],
+                main_phases_molar_fractions=self.main_phases_molar_fractions[
+                    key
+                ],
+                temperatures=self.temperatures[key],
+                alphas=self.alphas[key],
+                iterations=self.iterations[key],
+                specified_variable=self.specified_variable[key],
+            )
+        elif key == "alpha" or key == "a":
+            return self.alphas
+        elif key == "T":
+            return self.temperatures
+        elif key == "z":
+            return self.global_composition
+        elif key == "x":
+            return self.main_phases_compositions
+        elif key == "w":
+            return self.reference_phase_compositions
+
+    def __repr__(self):
+        return self.df.__repr__()
+
+    def __mul__(self, other):
+        return TXEnvelope(
+            global_composition_0=self.global_composition_0,
+            global_composition_i=self.global_composition_i,
+            pressure=self.pressure,
+            main_phases_compositions=self.main_phases_compositions * other,
+            reference_phase_compositions=self.reference_phase_compositions
+            * other,
+            main_phases_molar_fractions=self.main_phases_molar_fractions
+            * other,
+            temperatures=self.temperature * other,
             alphas=self.alphas * other,
             iterations=self.iterations,
             specified_variable=self.specified_variable,
