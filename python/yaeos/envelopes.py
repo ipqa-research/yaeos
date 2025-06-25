93a5ced27c659dabb5065e061919d176663b5b26
Federico E. Benelli <federico.benelli@unc.edu.ar>
2025-06-25 14:46:51 -0300

    prettier

--- python/yaeos/envelopes.py
+++ python/yaeos/envelopes.py
@@ -15,8 +15,47 @@ import pandas as pd
 
 class PTEnvelope:
     """PTEnvelope.
-
     This class represents a pressure-temperature envelope.
+    Attributes
+    ----------
+    global_composition : np.ndarray
+        The global composition of the system. Shape is (n_components,).
+    main_phases_compositions : np.ndarray
+        The compositions of the main phases along the envelope.
+        Shape is (n_points, n_phases, n_components).
+    reference_phase_compositions : np.ndarray
+        The compositions of the reference phase along the envelope.
+        Shape is (n_points, n_components).
+    main_phases_molar_fractions : np.ndarray
+        The molar fractions of the main phases along the envelope.
+        Shape is (n_points, n_phases).
+    pressures : np.ndarray
+        The pressures along the envelope. [bar]
+    temperatures : np.ndarray
+        The temperatures along the envelope. [K]
+    iterations : np.ndarray
+        The number of iterations taken to compute the envelope at each point.
+        Shape is (n_points,).
+    specified_variable : np.ndarray
+        The specified variable used to compute the envelope at each point.
+        Shape is (n_points,).
+    reference_phase_kinds : np.ndarray
+        The kinds of the reference phase at each point.
+        Shape is (n_points,).
+    main_phases_kinds : np.ndarray
+        The kinds of the main phases at each point.
+        Shape is (n_points, n_phases).
+    cp : list
+        A list of lists containing the indices of the critical points for each
+        phase. Each sublist corresponds to a phase and contains the indices of
+        the critical points in the `temperatures` and `pressures` arrays.
+    df : pd.DataFrame
+        A DataFrame containing the data of the envelope. The columns are:
+        - 'T': Temperatures along the envelope.
+        - 'P': Pressures along the envelope.
+        - 'x_i^j': Compositions of the main phases, where `i` is the component index and `j` is the phase index.
+        - 'w_i': Compositions of the reference phase, where `i` is the component index.
+        - 'beta^j': Molar fractions of the main phases, where `j` is the phase index.
     """
 
     def __init__(
@@ -74,6 +113,8 @@ class PTEnvelope:
 
             for i in range(1, len(lnK)):
                 if all(lnK[i, :] * lnK[i - 1, :] < 0):
+                    # TODO: There should be an interpolation here to find
+                    # a better approximation of the critical point.
                     cp_idx.append(i)
 
             idx.append(cp_idx)
@@ -88,10 +129,28 @@ class PTEnvelope:
         #     if crit:
         #         print("asd")
 
+    def plot(self, **plot_kwargs):
+        if "ax" in plot_kwargs:
+            ax = plot_kwargs["ax"]
+            del plot_kwargs["ax"]
+        else:
+            ax = plt.gca()
+        ax.plot(self.temperatures, self.pressures, **plot_kwargs)
+        ax.set_xlabel("Temperature [K]")
+        ax.set_ylabel("Pressure [bar]")
+        for cp in self.cp:
+            ax.scatter(
+                self.temperatures[cp], self.pressures[cp], color="black"
+            )
+
+    def __repr__(self):
+        display(self.df)
+        return ""
+
     def __getitem__(self, key):
         if "key" in self.__dict__:
             return self.__dict__["key"]
-        elif isinstance(key, np.ndarray):
+        elif isinstance(key, np.ndarray) or isinstance(key, list):
             return PTEnvelope(
                 global_composition=self.global_composition,
                 main_phases_compositions=self.main_phases_compositions[key],
@@ -101,6 +160,8 @@ class PTEnvelope:
                 main_phases_molar_fractions=self.main_phases_molar_fractions[
                     key
                 ],
+                main_phases_kinds=self.main_phases_kinds[key],
+                reference_phase_kinds=self.reference_phase_kinds[key],
                 pressures=self.pressures[key],
                 temperatures=self.temperatures[key],
                 iterations=self.iterations[key],
@@ -120,20 +181,6 @@ class PTEnvelope:
             return self.main_phases_compositions
         elif key == "w":
             return self.reference_phase_compositions
-
-    def plot(self, **plot_kwargs):
-        plt.plot(self.temperatures, self.pressures, **plot_kwargs)
-        plt.xlabel("Temperature [K]")
-        plt.ylabel("Pressure [bar]")
-        plt.title("PT Envelope")
-        for cp in self.cp:
-            plt.scatter(
-                self.temperatures[cp], self.pressures[cp], color="black"
-            )
-
-    def __repr__(self):
-        display(self.df)
-        return ""
 
     def __mul__(self, other):
         return PTEnvelope(
@@ -157,6 +204,49 @@ class PXEnvelope:
     """PXEnvelope.
 
     This class represents a pressure-composition envelope.
+
+    Attributes
+    ----------
+    temperature : float
+        The temperature at which the envelope is defined. [K]
+    global_composition_0 : np.ndarray
+        The global composition at the point where :math:`\alpha = 0`.
+    global_composition_i : np.ndarray
+        The global composition at the point where :math:`\alpha = 1`.
+    main_phases_compositions : np.ndarray
+        The compositions of the main phases along the envelope.
+        Shape is (n_points, n_phases, n_components).
+    reference_phase_compositions : np.ndarray
+        The compositions of the reference phase along the envelope.
+        Shape is (n_points, n_components).
+    main_phases_molar_fractions : np.ndarray
+        The molar fractions of the main phases along the envelope.
+        Shape is (n_points, n_phases).
+    pressures : np.ndarray
+        The pressures along the envelope. [bar]
+    alphas : np.ndarray
+        The molar fraction of the `global_composition_i`, :math:`\alpha`.
+        Shape is (n_points,).
+    iterations : np.ndarray
+        The number of iterations taken to compute the envelope at each point.
+        Shape is (n_points,).
+    specified_variable : np.ndarray
+        The specified variable used to compute the envelope at each point.
+        Shape is (n_points,).
+    cp : list
+        A list of lists containing the indices of the critical points for each
+        phase. Each sublist corresponds to a phase and contains the indices of
+        the critical points in the `pressures` and `alphas` arrays.
+    df : pd.DataFrame
+        A DataFrame containing the data of the envelope. The columns are:
+        - 'alpha': Molar fraction of the `global_composition_i`.
+        - 'P': Pressures along the envelope.
+        - 'x_i^j': Compositions of the main phases, where `i`
+        is the component index and `j` is the phase index.
+        - 'w_i': Compositions of the reference phase, where `i` is the
+        component index.
+        - 'beta^j': Molar fractions of the main phases, where `j`
+        is the phase index.
     """
 
     def __init__(
@@ -228,6 +318,31 @@ class PXEnvelope:
         #     if crit:
         #         print("asd")
 
+    def plot(self, **plot_kwargs):
+        """Plot the envelope.
+
+        Plot the envelope in a matplotlib axis. Using the natural variables of
+        the envelope (:math:`alpha` and pressure)  If the `ax` keyword argument
+        is provided, it will be used as the axis to plot on. Otherwise, the
+        current axis will be used.
+
+        Parameters
+        ----------
+        plot_kwargs : dict
+            Keyword arguments to pass to the `plot` method of the axis.
+            This can include line style, color, etc.
+        """
+        if "ax" in plot_kwargs:
+            ax = plot_kwargs["ax"]
+            del plot_kwargs["ax"]
+        else:
+            ax = plt.gca()
+        ax.plot(self.alphas, self.alphas, **plot_kwargs)
+        ax.set_xlabel(r"$\alpha$")
+        ax.set_ylabel("Pressure [bar]")
+        for cp in self.cp:
+            ax.scatter(self.pressures[cp], self.alphas[cp], color="black")
+
     def __getitem__(self, key):
         if "key" in self.__dict__:
             return self.__dict__["key"]
@@ -285,7 +400,52 @@ class PXEnvelope:
 class TXEnvelope:
     """TXEnvelope.
 
-    This class represents a pressure-composition envelope.
+    This class represents a temperature-composition envelope.
+
+    Attributes
+    ----------
+    pressure : float
+        The pressure at which the envelope is defined. [bar]
+    global_composition_0 : np.ndarray
+        The global composition at the point where :math:`\alpha = 0`.
+    global_composition_i : np.ndarray
+        The global composition at the point where :math:`\alpha = 1`.
+    main_phases_compositions : np.ndarray
+        The compositions of the main phases along the envelope.
+        Shape is (n_points, n_phases, n_components).
+    reference_phase_compositions : np.ndarray
+        The compositions of the reference phase along the envelope.
+        Shape is (n_points, n_components).
+    main_phases_molar_fractions : np.ndarray
+        The molar fractions of the main phases along the envelope.
+        Shape is (n_points, n_phases).
+    temperatures : np.ndarray
+        The temperatures along the envelope. [K]
+    alphas : np.ndarray
+        The molar fraction of the `global_composition_i`, :math:`\alpha`.
+        Shape is (n_points,).
+    iterations : np.ndarray
+        The number of iterations taken to compute the envelope at each point.
+        Shape is (n_points,).
+    specified_variable : np.ndarray
+        The specified variable used to compute the envelope at each point.
+        Shape is (n_points,).
+    temperature : float
+        The temperatures along envelope. [K]
+    cp : list
+        A list of lists containing the indices of the critical points for each
+        phase. Each sublist corresponds to a phase and contains the indices of
+        the critical points in the `temperatures` and `alphas` arrays.
+    df : pd.DataFrame
+        A DataFrame containing the data of the envelope. The columns are:
+        - 'alpha': Molar fraction of the `global_composition_i`.
+        - 'T': Temperatures along the envelope.
+        - 'x_i^j': Compositions of the main phases, where `i`
+        is the component index and `j` is the phase index.
+        - 'w_i': Compositions of the reference phase, where `i` is the
+        component index.
+        - 'beta^j': Molar fractions of the main phases, where `j`
+        is the phase index.
     """
 
     def __init__(
@@ -357,10 +517,35 @@ class TXEnvelope:
         #     if crit:
         #         print("asd")
 
+    def plot(self, **plot_kwargs):
+        """Plot the envelope.
+
+        Plot the envelope in a matplotlib axis. Using the natural variables of
+        the envelope (:math:`alpha` and temperature)  If the `ax` keyword
+        argument is provided, it will be used as the axis to plot on.
+        Otherwise, the current axis will be used.
+
+        Parameters
+        ----------
+        plot_kwargs : dict
+            Keyword arguments to pass to the `plot` method of the axis.
+            This can include line style, color, etc.
+        """
+        if "ax" in plot_kwargs:
+            ax = plot_kwargs["ax"]
+            del plot_kwargs["ax"]
+        else:
+            ax = plt.gca()
+        ax.plot(self.alphas, self.temperatures, **plot_kwargs)
+        ax.set_xlabel(r"$\alpha$")
+        ax.set_ylabel("Temperature [K]")
+        for cp in self.cp:
+            ax.scatter(self.alphas[cp], self.temperatures[cp], color="black")
+
     def __getitem__(self, key):
         if "key" in self.__dict__:
             return self.__dict__["key"]
-        elif isinstance(key, np.ndarray):
+        elif isinstance(key, np.ndarray) or isinstance(key, list):
             return TXEnvelope(
                 global_composition_0=self.global_composition_0,
                 global_composition_i=self.global_composition_i,
