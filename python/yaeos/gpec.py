c263bd2ac3923a4d9372c1c84abb0c08be70b1ca
fedebenelli <fedebenelli@outlook.com>
2025-12-18 17:16:17 -0300

    gpec

--- python/yaeos/gpec.py
+++ python/yaeos/gpec.py
@@ -191,7 +191,15 @@ class GPEC:
         plt.xlabel("Temperature (K)")
         plt.ylabel("Pressure (bar)")
 
-    def calc_pxy(self, temperature, a0=1e-5):
+    def calc_pxy(
+        self,
+        temperature,
+        x10=0.9999,
+        x20=0.9999,
+        dx10=1e-3,
+        dx20=-1e-3,
+        dxll0=1e-4,
+    ):
         """Calculate a Pxy phase diagram.
 
         This method calculates the Pxy phase diagram for the system at a given
@@ -202,8 +210,21 @@ class GPEC:
         ----------
         temperature : float
             The temperature at which to calculate the Pxy phase diagram.
-        a0 : float, optional
-            A parameter used in the calculation (default is 1e-5).
+        x10 : float, optional
+            Initial molar fraction of the first component for the line that
+            starts from the almost pure first component (default is 1e-5).
+        x20 : float, optional
+            Initial molar fraction of the second component for the line that
+            starts from the almost pure second component (default is 1e-5).
+        dx10 : float, optional
+            Step size for the line that starts from the almost pure first
+            component (default is 1e-3).
+        dx20 : float, optional
+            Step size for the line that starts from the almost pure second
+            component (default is 1e-3).
+        dxll0 : float, optional
+            Step size for the line that starts from the liquid-liquid critical
+            point (default is 1e-4).
 
         Returns
         -------
@@ -227,8 +248,8 @@ class GPEC:
             temperature=temperature,
             kind="bubble",
             p0=p0,
-            a0=a0,
-            ds0=1e-3,
+            a0=x10,
+            ds0=dx10,
             max_points=MAX_POINTS,
         )
 
@@ -242,16 +263,16 @@ class GPEC:
                 temperature=temperature,
                 kind="bubble",
                 p0=p0,
-                a0=1-a0,
-                ds0=-1e-3,
+                a0=1 - x20,
+                ds0=dx20,
                 max_points=MAX_POINTS,
             )
-        
+
         if self._cl_ll:
             loc = np.argmin(abs(self._cl_ll["T"] - temperature))
             p0, t = self._cl_ll["P"][loc], self._cl_ll["T"][loc]
             if abs(t - temperature) < 1 or temperature > self._cep_ll["T"]:
-                
+
                 a = self._cl_ll["a"][loc]
                 z = a * self._zi + (1 - a) * self._z0
                 x_l0 = [z.copy()]
@@ -274,7 +295,7 @@ class GPEC:
                     p0=p0,
                     alpha0=a + 1e-5,
                     ns0=len(z) + 3,
-                    ds0=1e-4,
+                    ds0=dxll0,
                     max_points=MAX_POINTS,
                 )
 
@@ -282,7 +303,16 @@ class GPEC:
 
         return pxs
 
-    def plot_pxy(self, temperature, a0=1e-5):
+    def plot_pxy(
+        self,
+        temperature,
+        x10=1e-5,
+        x20=1e-5,
+        dx10=1e-3,
+        dx20=1e-3,
+        dxll0=1e-4,
+        **plot_kwargs,
+    ):
         """Plot a Pxy phase diagram.
 
         This method plots the Pxy phase diagram for the system at a given
@@ -293,22 +323,53 @@ class GPEC:
         ----------
         temperature : float
             The temperature at which to plot the Pxy phase diagram.
-
-        a0 : float, optional
-            Molar fraction of the first or second component in the
-            initial point (default is 1e-5).
+        x10 : float, optional
+            Initial molar fraction of the first component for the line that
+            starts from the almost pure first component (default is 1e-5).
+        x20 : float, optional
+            Initial molar fraction of the second component for the line that
+            starts from the almost pure second component (default is 1e-5).
+        dx10 : float, optional
+            Step size for the line that starts from the almost pure first
+            component (default is 1e-3).
+        dx20 : float, optional
+            Step size for the line that starts from the almost pure second
+            component (default is 1e-3).
+        dxll0 : float, optional
+            Step size for the line that starts from the liquid-liquid critical
+            point (default is 1e-4).
+        plot_kwargs : dict, optional
+            Additional keyword arguments to be passed to the plot function.
         """
-        pxs = self.calc_pxy(temperature, a0=a0)
+        pxs = self.calc_pxy(
+            temperature, x10=x10, x20=x20, dx10=dx10, dx20=dx20, dxll0=dxll0
+        )
 
         for px in pxs:
             if px:
-                plt.plot(px.main_phases_compositions[:, 0, 0], px["P"])
-                plt.plot(px.reference_phase_compositions[:, 0], px["P"])
+                plt.plot(
+                    px.main_phases_compositions[:, 0, 0],
+                    px["P"],
+                    **plot_kwargs,
+                )
+                plt.plot(
+                    px.reference_phase_compositions[:, 0],
+                    px["P"],
+                    **plot_kwargs,
+                )
 
         plt.xlabel(r"$x_1$, $y_1$")
         plt.ylabel("Pressure (bar)")
 
-    def calc_txy(self, pressure, a0=1e-5):
+    def calc_txy(
+        self,
+        pressure,
+        y10=0.9999,
+        y20=0.9999,
+        dy10=-1e-3,
+        dy20=1e-3,
+        dyll0=1e-4,
+    ):
         """Calculate a Txy phase diagram.
 
         This method calculates the Txy phase diagram for the system at a given
@@ -319,17 +380,30 @@ class GPEC:
         ----------
         pressure : float
             The pressure at which to calculate the Txy phase diagram.
-        a0 : float, optional
-            Molar fraction of the first or second component in the
-            initial point (default is 1e-5).
+        y10 : float, optional
+            Initial molar fraction of the first component for the line that
+            starts from the almost pure first component (default is 1e-5).
+        y20 : float, optional
+            Initial molar fraction of the second component for the line that
+            starts from the almost pure second component (default is 1e-5).
+        dy10 : float, optional
+            Step size for the line that starts from the almost pure first
+            component (default is -1e-3).
+        dy20 : float, optional
+            Step size for the line that starts from the almost pure second
+            component (default is 1e-3).
+        dyll0 : float, optional
+            Step size for the line that starts from the liquid-liquid critical
+            point (default is 1e-4).
 
         Returns
         -------
         list
             A list containing the calculated Txy envelopes. They are sorted
-            in the order of: starting from the almost pure first component,
-            starting from the almost pure second component, and starting from
-            liquid-liquid critical line if it exists.
+            in the order of:
+            - Starting from the almost pure first component.
+            - Starting from the almost pure second component.
+            - Starting from liquid-liquid critical line if it exists.
         """
         psat_1, psat_2 = self._pures
 
@@ -346,7 +420,7 @@ class GPEC:
                 pressure=pressure,
                 kind="dew",
                 t0=t0,
-                a0=a0,
+                a0=1 - y20,
                 ds0=1e-5,
                 max_points=MAX_POINTS,
             )
@@ -361,11 +435,10 @@ class GPEC:
                 pressure=pressure,
                 kind="bubble",
                 t0=t0,
-                a0=1 - a0,
-                ds0=-1e-5,
+                a0=y10,
+                ds0=dy10,
                 max_points=MAX_POINTS,
             )
-
         if self._cl_ll:
             loc = np.argmin(abs(self._cl_ll["P"] - pressure))
             t0, p = self._cl_ll["T"][loc], self._cl_ll["P"][loc]
@@ -393,7 +466,7 @@ class GPEC:
                     t0=t0,
                     alpha0=a + 1e-5,
                     ns0=len(z) + 3,
-                    ds0=1e-4,
+                    ds0=dyll0,
                     max_points=MAX_POINTS,
                 )
 
@@ -401,7 +474,16 @@ class GPEC:
 
         return txs
 
-    def plot_txy(self, pressure, a0=1e-5):
+    def plot_txy(
+        self,
+        pressure,
+        y10=0.9999,
+        y20=0.9999,
+        dy10=-1e-3,
+        dy20=1e-3,
+        dyll0=1e-4,
+        **plot_kwargs,
+    ):
         """Plot a Txy phase diagram.
 
         This method plots the Txy phase diagram for the system at a given
@@ -411,16 +493,40 @@ class GPEC:
         ----------
         pressure : float
             The pressure at which to plot the Txy phase diagram.
-        a0 : float, optional
-            Molar fraction of the first or second component in the
-            initial point (default is 1e-5).
+        y10 : float, optional
+            Initial molar fraction of the first component for the line that
+            starts from the almost pure first component (default is 0.9999).
+        y20 : float, optional
+            Initial molar fraction of the second component for the line that
+            starts from the almost pure second component (default is 0.9999).
+        dy10 : float, optional
+            Step size for the line that starts from the almost pure first
+            component (default is -1e-3).
+        dy20 : float, optional
+            Step size for the line that starts from the almost pure second
+            component (default is 1e-3).
+        dyll0 : float, optional
+            Step size for the line that starts from the liquid-liquid critical
+            point (default is 1e-4).
+        plot_kwargs : dict, optional
+            Additional keyword arguments to be passed to the plot function.
         """
-        txs = self.calc_txy(pressure, a0=a0)
+        txs = self.calc_txy(
+            pressure, y10=y10, y20=y20, dy10=dy10, dy20=dy20, dyll0=dyll0
+        )
 
         for tx in txs:
             if tx:
-                plt.plot(tx.main_phases_compositions[:, 0, 0], tx["T"])
-                plt.plot(tx.reference_phase_compositions[:, 0], tx["T"])
+                plt.plot(
+                    tx.main_phases_compositions[:, 0, 0],
+                    tx["T"],
+                    **plot_kwargs,
+                )
+                plt.plot(
+                    tx.reference_phase_compositions[:, 0],
+                    tx["T"],
+                    **plot_kwargs,
+                )
 
         plt.xlabel(r"$x_1$, $y_1$")
         plt.ylabel("Temperature (K)")
