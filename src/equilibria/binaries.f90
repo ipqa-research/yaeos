f7af462d0a3036478834a9f9f42cf9e738e854d7
fedebenelli <fedebenelli@outlook.com>
2025-12-23 10:08:19 -0300

    now?

--- src/equilibria/binaries.f90
+++ src/equilibria/binaries.f90
@@ -25,6 +25,23 @@ module yaeos__equilibria_binaries
       real(pr), allocatable :: P(:) !! Pressure [bar]
    end type BinaryThreePhase
 
+   type :: LLVVarEnum
+      integer :: x1=1
+      integer :: x2=2
+      integer :: y1=3
+      integer :: y2=4
+      integer :: w1=5
+      integer :: w2=6
+      integer :: Vx=7
+      integer :: Vy=8
+      integer :: Vw=9
+      integer :: T=10
+   end type LLVVarEnum
+
+   type(LLVVarEnum), parameter :: llv_vars = LLVVarEnum()
+
+   integer, parameter :: n_llv_vars = 10
+
 contains
 
    subroutine find_llcl(model, z0, zi, P, a, V, T)
@@ -103,20 +120,22 @@ contains
       !! corresponding system of equations (defined at [[three_phase_line_F]])
       !! at each point.
       !! To trace the whole line a continuation method is used to obtain good
-      !! initial guesses for each point. 
+      !! initial guesses for each point.
       !! The specification used to trace the line is initially the difference
       !! between the mole fractions of the two liquid phases, and then it is
       !! switched to temperature.
       class(ArModel), intent(in) :: model !! Thermodynamic model to use
       type(EquilibriumState), intent(in) :: cep !! Converged critical end point.
 
-      real(pr) :: X(7)
-      real(pr) :: dFdS(7), F(7), dF(7, 7)
+      integer, parameter :: nvars = 10
+
+      real(pr) :: X(nvars)
+      real(pr) :: dFdS(nvars), F(nvars), dF(nvars, nvars)
       integer :: ns
-      real(pr) ::  dS, S, dXdS(7)
+      real(pr) ::  dS, S, dXdS(nvars)
 
       real(pr) :: T, P, Vx, Vy, Vw
-      real(pr) :: x1, y1, w1
+      real(pr) :: xx(2), y(2), w(2)
 
       real(pr), allocatable :: Ts(:), Ps(:)
 
@@ -126,22 +145,29 @@ contains
       real(pr) :: delta
 
       dFdS = 0
-      dFdS(7) = -1
+      dFdS(10) = -1
 
-      delta = 0.025 * cep%x(1)
-      x1 = cep%x(1) + delta
-      y1 = cep%x(1) - delta
-      w1 = cep%y(1)
+      delta = 0.0001 * cep%x(1)
+      xx(1) = cep%x(1) - delta
+      y(1) = cep%x(1) + delta
+      w(1) = cep%y(1)
+
+      xx(2) = 1 - xx(1)
+
+      y(2) = 1 - y(1)
+      w(2) = 1 - w(1)
+
       T = cep%T
       P = cep%P
-      call model%volume([x1, 1 - x1], P, T, Vx, root_type="liquid")
-      call model%volume([y1, 1 - y1], P, T, Vy, root_type="liquid")
+
+      call model%volume(xx, P, T, Vx, root_type="liquid")
+      call model%volume(y, P, T, Vy, root_type="liquid")
       Vw = cep%Vy
 
       X = log([&
-         x1, &
-         y1, &
-         w1, &
+         xx, &
+         y, &
+         w, &
          Vx, &
          Vy, &
          Vw, &
@@ -149,19 +175,15 @@ contains
          ])
       T = HUGE(1._pr)
       ns = 0
-      if (ns == 0) then
-         S = exp(X(2)) - exp(X(1))
-      else if (ns == -1) then
-         S = X(4) - X(5)
-      else
-         S = X(ns)
-      end if
-      S = y1 - x1 - 2*delta
-      dS = 0.0001
-
-      ns = -1
-      S = exp(X(4)) - exp(X(5))
-      dS = 1e-11
+      ! if (ns == 0) then
+      !    S = exp(X(2)) - exp(X(1))
+      ! else if (ns == -1) then
+      !    S = X(4) - X(5)
+      ! else
+      !    S = X(ns)
+      ! end if
+      S = xx(1) - y(1) - delta
+      dS = -0.0001
 
       points = 0
       F = 0
@@ -169,50 +191,54 @@ contains
       allocate(&
          llv%T(0), llv%P(0), llv%x1(0), llv%y1(0), llv%w1(0), &
          llv%Vx(0), llv%Vy(0), llv%Vw(0))
-      do while(T > 100 .and. P > 1e-8_pr .and. maxval(abs(F)) < 1e-9)
+      do while((T > 100 .or. P > 1e-8_pr) .and. maxval(abs(F)) < 1e-9)
          points = points + 1
          call three_phase_line_F_solve(model, X, ns, S, F, dF, iters)
+         if (iters >= 100) then
+            X = X - 0.9 * dXdS * dS 
+            S = S - 0.9 * dS
+            call three_phase_line_F_solve(model, X, ns, S, F, dF, iters)
+         end if
 
-         x1 = exp(X(1))
-         y1 = exp(X(2))
-         w1 = exp(X(3))
-         Vx = exp(X(4))
-         Vy = exp(X(5))
-         Vw = exp(X(6))
-         T = exp(X(7))
-         call model%pressure([x1, 1-x1], Vx, T, P)
-         
+         xx = exp(X(llv_vars%x1:llv_vars%x2))
+         y = exp(X(llv_vars%y1:llv_vars%y2))
+         w = exp(X(llv_vars%w1:llv_vars%w2))
+         Vx = exp(X(llv_vars%Vx))
+         Vy = exp(X(llv_vars%Vy))
+         Vw = exp(X(llv_vars%Vw))
+         T = exp(X(llv_vars%T))
+         call model%pressure(xx, Vx, T, P)
+
          if (maxval(abs(F)) < 1e-9 .and. P > 0) then
             llv%T = [llv%T, T]
             llv%P = [llv%P, P]
-            llv%x1 = [llv%x1, x1]
-            llv%y1 = [llv%y1, y1]
-            llv%w1 = [llv%w1, w1]
+            llv%x1 = [llv%x1, xx(1)]
+            llv%y1 = [llv%y1, y(1)]
+            llv%w1 = [llv%w1, w(1)]
             llv%Vx = [llv%Vx, Vx]
             llv%Vy = [llv%Vy, Vy]
             llv%Vw = [llv%Vw, Vw]
          end if
 
-         ns = -1
-         dS = -0.01 * 3. / real(iters, pr)
 
          dXdS = solve_system(dF, -dFdS)
 
-         if (T < llv%T(1) - 10) then
-            ns = 7
-            dS = -0.01 * 3. / real(iters, pr)
-            dXdS = dXdS / dXdS(ns)
-         end if
+         ns = llv_vars%y1
+         dS = dXdS(ns) * dS
+         dXdS = dXdS / dXdS(ns)
 
-         ! if (P < 1) then
-         !    ns = -1
-         !    dS = -1e-5
+         ! if (T < llv%T(1) - 10) then
+         !    ns = 7
+         !    dS = dXdS(ns) * dS
+         !    dS = -0.001 * 3. / real(iters, pr)
+         !    ! dS = sign(max(abs(dS), 0.001_pr), dS)
+         !    dXdS = dXdS / dXdS(ns)
          ! end if
 
          do while(&
-              abs(Vx - exp(X(4) + dS*dXdS(4))) > 0.5*Vx&
-              .or. abs(Vy - exp(X(5) + dS*dXdS(5))) > 0.5*Vy&
-              .or. abs(Vw - exp(X(6) + dS*dXdS(6))) > 0.5*Vw&
+            abs(Vx - exp(X(llv_vars%Vx) + dS*dXdS(llv_vars%Vx))) > 0.5*Vx&
+            .or. abs(Vy - exp(X(llv_vars%Vy) + dS*dXdS(llv_vars%Vy))) > 0.5*Vy&
+            .or. abs(Vw - exp(X(llv_vars%Vw) + dS*dXdS(llv_vars%Vw))) > 0.5*Vw&
             )
             dS = dS/2
          end do
@@ -220,9 +246,9 @@ contains
          X = X + dXdS * dS
 
          if (ns == 0) then
-            S = exp(X(2)) - exp(X(1))
+            S = exp(X(llv_vars%x1)) - exp(X(llv_vars%y1))
          else if (ns == -1) then
-            S = X(4) - X(5)
+            S = X(llv_vars%Vx) - X(llv_vars%Vy)
          else
             S = X(ns)
          end if
@@ -244,7 +270,7 @@ contains
       !! & \ln \phi_i^{(y)} - \ln \phi_i^{(w)} = 0 \\
       !! & P^{(x)} - P^{(w)} = 0 \\
       !! & P^{(y)} - P^{(w)} = 0 \\
-      !! & \text{specification} 
+      !! & \text{specification}
       !! \begin{cases}
       !! y_1 - x_1 - S = 0 & \text{if } ns = 0 \\
       !! \ln v_x - \ln v_y - S = 0 & \text{if } ns = -1 \\
@@ -271,7 +297,7 @@ contains
       real(pr) :: x1, y1, w1
       real(pr) :: vx, vy, vw, T
       real(pr) :: x(2), y(2), w(2)
-      
+
       real(pr) :: Px, Py, Pw
       real(pr) :: isofug_1(2), isofug_2(2)
       real(pr) :: Peq_1, Peq_2
@@ -282,23 +308,24 @@ contains
       real(pr) :: dlnfxdP(2), dlnfydP(2), dlnfwdP(2)
       real(pr) :: dlnfxdT(2), dlnfydT(2), dlnfwdT(2)
 
-
       real(pr) :: dPxdN(2), dPydN(2), dPwdN(2)
       real(pr) :: dPxdV, dPydV, dPwdV
       real(pr) :: dPxdT, dPydT, dPwdT
       real(pr) :: dxdn(2, 2)
 
-      x1 = exp(XVars(1))
-      y1 = exp(XVars(2))
-      w1 = exp(XVars(3))
-      vx = exp(Xvars(4))
-      vy = exp(Xvars(5))
-      vw = exp(Xvars(6))
-      T =  exp(Xvars(7))
+      x = exp(XVars(llv_vars%x1:llv_vars%x2))
+      y = exp(XVars(llv_vars%y1:llv_vars%y2))
+      w = exp(XVars(llv_vars%w1:llv_vars%w2))
+      vx = exp(Xvars(llv_vars%Vx))
+      vy = exp(Xvars(llv_vars%Vy))
+      vw = exp(Xvars(llv_vars%Vw))
+      T =  exp(Xvars(llv_vars%T))
+      x1 = x(1)
+      y1 = y(1)
 
-      x = [x1, 1-x1]
-      y = [y1, 1-y1]
-      w = [w1, 1-w1]
+      ! x = [x1, 1-x1]
+      ! y = [y1, 1-y1]
+      ! w = [w1, 1-w1]
 
       call model%lnfug_vt(&
          x, Vx, T, Px, lnf_x, &
@@ -327,84 +354,119 @@ contains
       F(3:4) = isofug_2
       F(5) = Peq_1
       F(6) = Peq_2
+      F(7) = sum(x) - 1
+      F(8) = sum(y) - 1
+      F(9) = sum(w) - 1
 
       if (ns == 0) then
-         F(7) = y1 - x1 - S
+         F(10) = y1 - x1 - S
       else if (ns == -1) then
-         F(7) = log(vx / vy) - S
+         F(10) = log(vx / vy) - S
       else
-         F(7) = Xvars(ns) - S
+         F(10) = Xvars(ns) - S
       end if
 
       df = 0
 
-      df(1, 1) = dlnfxdn(1, 1) - dlnfxdn(1, 2)
-      df(2, 1) = dlnfxdn(2, 1) - dlnfxdn(2, 2)
-      df(3:4, 1) = 0
-      df(5, 1) = dPxdN(1) - dPxdN(2)
-      df(6, 1) = 0
+      ! Derivatives wrt x1
+      df(1, llv_vars%x1) = dlnfxdn(1, 1)
+      df(2, llv_vars%x1) = dlnfxdn(2, 1)
+      df(3:4, llv_vars%x1) = 0
+      df(5, llv_vars%x1) = dPxdN(1)
+      df(6, llv_vars%x1) = 0
+      df(7, llv_vars%x1) = 1
+
+      ! Derivatives wrt x2
+      df(1, llv_vars%x2) = dlnfxdn(1, 2)
+      df(2, llv_vars%x2) = dlnfxdn(2, 2)
+      df(3:4, llv_vars%x2) = 0
+      df(5, llv_vars%x2) = dPxdN(2)
+      df(6, llv_vars%x2) = 0
+      df(7, llv_vars%x2) = 1
 
       ! Derivatives wrt y1
-      df(1:2, 2) = 0
-      df(3, 2) = dlnfydn(1, 1) - dlnfydn(1, 2)
-      df(4, 2) = dlnfydn(2, 1) - dlnfydn(2, 2)
-      df(5, 2) = 0
-      df(6, 2) = dPydN(1) - dPydN(2)
+      df(1:2, llv_vars%y1) = 0
+      df(3, llv_vars%y1) = dlnfydn(1, 1)
+      df(4, llv_vars%y1) = dlnfydn(2, 1)
+      df(5, llv_vars%y1) = 0
+      df(6, llv_vars%y1) = dPydN(1)
+      df(8, llv_vars%y1) = 1
+
+      ! Derivatives wrt y2
+      df(1:2, llv_vars%y2) = 0
+      df(3, llv_vars%y2) = dlnfydn(1, 2)
+      df(4, llv_vars%y2) = dlnfydn(2, 2)
+      df(5, llv_vars%y2) = 0
+      df(6, llv_vars%y2) = dPydN(2)
+      df(8, llv_vars%y2) = 1
 
       ! Derivatives wrt w1
-      df(1, 3) =  - (dlnfwdn(1, 1) - dlnfwdn(1, 2))
-      df(2, 3) =  - (dlnfwdn(2, 1) - dlnfwdn(2, 2))
-      df(3, 3) =  - (dlnfwdn(1, 1) - dlnfwdn(1, 2))
-      df(4, 3) =  - (dlnfwdn(2, 1) - dlnfwdn(2, 2))
-      df(5, 3) = - (dPwdN(1) - dPwdN(2))
-      df(6, 3) = - (dPwdN(1) - dPwdN(2))
+      df(1, llv_vars%w1) =  - dlnfwdn(1, 1)
+      df(2, llv_vars%w1) =  - dlnfwdn(2, 1)
+      df(3, llv_vars%w1) =  - dlnfwdn(1, 1)
+      df(4, llv_vars%w1) =  - dlnfwdn(2, 1)
+      df(5, llv_vars%w1) = - dPwdN(1)
+      df(6, llv_vars%w1) = - dPwdN(1)
+      df(9, llv_vars%w1) = 1
 
-      ! Derivatives wrt vx
-      df(1, 4) = dlnfxdv(1)
-      df(2, 4) = dlnfxdv(2)
-      df(3:4, 4) = 0
-      df(5, 4) = dPxdV
-      df(6, 4) = 0
+      ! Derivatives wrt w2
+      df(1, llv_vars%w2) =  - dlnfwdn(1, 2)
+      df(2, llv_vars%w2) =  - dlnfwdn(2, 2)
+      df(3, llv_vars%w2) =  - dlnfwdn(1, 2)
+      df(4, llv_vars%w2) =  - dlnfwdn(2, 2)
+      df(5, llv_vars%w2) = - dPwdN(2)
+      df(6, llv_vars%w2) = - dPwdN(2)
+      df(9, llv_vars%w2) = 1
 
-      ! Derivatives wrt vy
-      df(1:2, 5) = 0
-      df(3, 5) = dlnfydv(1)
-      df(4, 5) = dlnfydv(2)
-      df(5, 5) = 0
-      df(6, 5) = dPydV
+      ! Derivatives wrt Vx
+      df(1, llv_vars%Vx) = dlnfxdv(1)
+      df(2, llv_vars%Vx) = dlnfxdv(2)
+      df(3:4, llv_vars%Vx) = 0
+      df(5, llv_vars%Vx) = dPxdV
+      df(6, llv_vars%Vx) = 0
+
+      ! Derivatives wrt Vy
+      df(1:2, llv_vars%Vy) = 0
+      df(3, llv_vars%Vy) = dlnfydv(1)
+      df(4, llv_vars%Vy) = dlnfydv(2)
+      df(5, llv_vars%Vy) = 0
+      df(6, llv_vars%Vy) = dPydV
 
       ! Derivatives wrt vw
-      df(1, 6) = -dlnfwdv(1)
-      df(2, 6) = -dlnfwdv(2)
-      df(3, 6) = -dlnfwdv(1)
-      df(4, 6) = -dlnfwdv(2)
-      df(5, 6) = -dPwdV
-      df(6, 6) = -dPwdV
+      df(1, llv_vars%Vw) = -dlnfwdv(1)
+      df(2, llv_vars%Vw) = -dlnfwdv(2)
+      df(3, llv_vars%Vw) = -dlnfwdv(1)
+      df(4, llv_vars%Vw) = -dlnfwdv(2)
+      df(5, llv_vars%Vw) = -dPwdV
+      df(6, llv_vars%Vw) = -dPwdV
 
       ! Derivatives wrt T
-      df(1:2, 7) = dlnfxdT - dlnfwdT
-      df(3:4, 7) = dlnfydT - dlnfwdT
-      df(5, 7) = dPxdT - dPwdT
-      df(6, 7) = dPydT - dPwdT
+      df(1:2, llv_vars%T) = dlnfxdT - dlnfwdT
+      df(3:4, llv_vars%T) = dlnfydT - dlnfwdT
+      df(5, llv_vars%T) = dPxdT - dPwdT
+      df(6, llv_vars%T) = dPydT - dPwdT
 
       if (ns == 0) then
-         df(7, 1) = -1
-         df(7, 2) = 1
+         df(10, llv_vars%x1) = -1
+         df(10, llv_vars%y1) = 1
       else if (ns == -1) then
-         df(7, 4) = 1/vx
-         df(7, 5) = -1/vy
+         df(10, llv_vars%Vx) = 1/vx
+         df(10, llv_vars%Vy) = -1/vy
       else
-         df(7, ns) = 1
+         df(10, ns) = 1
       end if
 
       ! Multiply by variable values due to log-transform
-      df(:, 1) = df(:, 1) * x1
-      df(:, 2) = df(:, 2) * y1
-      df(:, 3) = df(:, 3) * w1
-      df(:, 4) = df(:, 4) * vx
-      df(:, 5) = df(:, 5) * vy
-      df(:, 6) = df(:, 6) * vw
-      df(:, 7) = df(:, 7) * T
+      df(:, llv_vars%x1) = df(:, llv_vars%x1) * x(1)
+      df(:, llv_vars%x2) = df(:, llv_vars%x2) * x(2)
+      df(:, llv_vars%y1) = df(:, llv_vars%y1) * y(1)
+      df(:, llv_vars%y2) = df(:, llv_vars%y2) * y(2)
+      df(:, llv_vars%w1) = df(:, llv_vars%w1) * w(1)
+      df(:, llv_vars%w2) = df(:, llv_vars%w2) * w(2)
+      df(:, llv_vars%Vx) = df(:, llv_vars%Vx) * vx
+      df(:, llv_vars%Vy) = df(:, llv_vars%Vy) * vy
+      df(:, llv_vars%Vw) = df(:, llv_vars%Vw) * vw
+      df(:, llv_vars%T) = df(:, llv_vars%T) * T
    end subroutine three_phase_line_F
 
    subroutine three_phase_line_F_solve(model, X, ns, S, F, dF, iters)
@@ -414,7 +476,7 @@ contains
       !! Solve the system of equations defined in `three_phase_line_F` using
       !! a Newton-Raphson method.
       !! It will make a maximum of 50 iterations to converge. With a tolerance
-      !! of 1e-9 in the maximum absolute value of the function vector or the 
+      !! of 1e-9 in the maximum absolute value of the function vector or the
       !! step vector.
       use yaeos__math, only: solve_system
       class(ArModel), intent(in) :: model !! Thermodynamic model to use
@@ -433,13 +495,12 @@ contains
       real(kind=pr) :: Xold(size(X))
 
       tol = 1e-9_pr
-      max_tries = 1000
+      max_tries = 100
 
       dX = 10
       F = 10
       do i = 1, max_tries
          call three_phase_line_F(model, X, ns, S, F, dF)
-
          res_norm = maxval(abs(F))
          if (res_norm < tol .or. maxval(abs(dX)) < 1e-9) exit
 
@@ -452,11 +513,17 @@ contains
             dX = dX / 2
          end do
 
+         ! if (exp(X(1) + dX(1)) < 0 .or. X(1) + dX(1) > 0) then
+         !    dX(1) = log(1e-15_pr) - X(1)
+         ! end if
+         ! if (exp(X(2) + dX(2)) < 0 .or. X(2) + dX(2) > 0) then
+         !    dX(2) = log(1e-15_pr) - X(2)
+         ! end if
+
          X = Xold + dX
 
-         if (present(iters)) iters = i
 
-         if (any(isnan(F))) error stop
+         if (present(iters)) iters = i
       end do
    end subroutine three_phase_line_F_solve
 end module yaeos__equilibria_binaries
