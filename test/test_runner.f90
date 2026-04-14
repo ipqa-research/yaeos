82fe4d77a83cfbe8734c944da4a06ce6b640e99b
Federico E. Benelli <federico.benelli@unc.edu.ar>
2026-04-14 15:02:48 -0300

    test alphas

--- test/test_runner.f90
+++ test/test_runner.f90
@@ -4,7 +4,6 @@ program tester
 
     use fortime, only: Timer
 
-    use test_cubic_alphas, only: suite_alphas => collect_suite
     use test_cubic_implementations, only: suite_implementations => collect_suite
     use test_cubic_mixrules, only: suite_cubic_mixrules => collect_suite
     use test_autodiff_api, only: suite_autodiff_hd => collect_suite
@@ -44,7 +43,6 @@ program tester
     stat = 0
 
     testsuites = [ &
-        new_testsuite("Alphas", suite_alphas), &
         new_testsuite("Cubic EoS", suite_implementations), &
         new_testsuite("Cubic MixRules", suite_cubic_mixrules), &
         new_testsuite("Autodiff APIs", suite_autodiff_hd), &
