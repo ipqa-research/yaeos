6489ea0ad06b87c14c3fd2877e832fc5e2d7db23
Federico E. Benelli <federico.benelli@unc.edu.ar>
2026-05-13 16:15:01 -0300

    first draft

---
+++ src/models/residual_helmholtz/cubic/cubic_sddlc.f90
@@ -0,0 +1,31 @@
+module yaeos__models_ar_cubic_sddlc
+   !! # Cubic EoS with segmented-DDLC mixing rule module
+   !!
+   !! This module contains the implementation of a generic CubicEoS using the
+   !! segmented Density Dependent Local Compositions mixing rule.
+   use yaeos__constants, only: pr
+   use yaeos__autodiff, only: hyperdual, ArModelAdiff
+
+   type, extends(ArModelAdiff) :: CubicEOSsDDLC
+   contains
+      procedure :: Ar => Ar
+      procedure :: get_v0 => get_v0
+   end type CubicEOSsDDLC
+
+contains
+
+   type(hyperdual) function Ar(self, n, V, T)
+      class(CubicEOSsDDLC) :: self !! Model
+      type(hyperdual), intent(in) :: n(:) !! Vector of moles
+      type(hyperdual), intent(in) :: V !! Volume [L]
+      type(hyperdual), intent(in) :: T !! Temperature [K]
+   end function Ar
+
+   real(pr) function get_v0(self, n, P, T)
+      class(CubicEOSsDDLC) :: self !! Model
+      real(pr), intent(in) :: n(:) !! Vector of moles
+      real(pr), intent(in) :: P !! Pressure [bar]
+      real(pr), intent(in) :: T !! Temperature [K]
+   end function get_v0
+
+end module yaeos__models_ar_cubic_sddlc
