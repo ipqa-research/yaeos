867d6ea1dee2a1ef58fa7e44a1bdc825b4690e29
Federico E. Benelli <federico.benelli@unc.edu.ar>
2026-06-03 16:10:56 -0300

    version

--- src/yaeos.f90
+++ src/yaeos.f90
@@ -15,5 +15,5 @@ module yaeos
    use yaeos__substance
    use yaeos__models
    use yaeos__equilibria
-   character(len=*), parameter :: version="4.5.2" !! This version.
+   character(len=*), parameter :: version="4.5.3" !! This version.
 end module yaeos
