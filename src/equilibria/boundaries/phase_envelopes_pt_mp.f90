e5ceb0a9515a597e00d7ab357533a347d66024a9
fedebenelli <fedebenelli@outlook.com>
2026-08-13 21:15:35 -0300

    stop criteria

--- src/equilibria/boundaries/phase_envelopes_pt_mp.f90
+++ src/equilibria/boundaries/phase_envelopes_pt_mp.f90
@@ -333,7 +333,7 @@ contains
             P > max_P&
             .or. P < 1e-5_pr &
             .or. abs(dS) <= 1e-14 &
-            .or. (T < 200._pr .and. dXdS(iT)*dS < 0) &
+            .or. (T < 200._pr .and. dXdS(iT)*dS < 0 .and. dXdS(iP) < 0) &
             ) exit continuation
 
          if (.not. anb) then
