68071d36a5fcdf39798b7beadc1936cfdb47b199
fedebenelli <fedebenelli@outlook.com>
2026-08-17 19:48:03 -0300

    tapeapi

--- src/adiff/autodiff_api/tapenade_ar_api.f90
+++ src/adiff/autodiff_api/tapenade_ar_api.f90
@@ -1,5 +1,5 @@
 module yaeos__tapenade_ar_api
-   !! Module that wraps tapenade generated routines to calculate
+   !! Module that wraps tapenade generated routines to calculate !
    !! Ar and derivatives.
    use yaeos__constants, only: pr
    use yaeos__models_ar, only: ArModel
@@ -17,7 +17,7 @@ module yaeos__tapenade_ar_api
       procedure(tapenade_ar_d_b), deferred :: ar_d_b
       procedure(tapenade_ar_d_d), deferred :: ar_d_d
       procedure :: residual_helmholtz => residual_helmholtz
-   end type
+   end type ArModelTapenade
 
    abstract interface
       subroutine tapenade_ar(model, n, v, t, arval)
@@ -25,7 +25,7 @@ module yaeos__tapenade_ar_api
          class(ArModelTapenade), intent(in) :: model
          real(pr), intent(in) :: n(:), v, t
          real(pr), intent(out) :: arval
-      end subroutine
+      end subroutine tapenade_ar
 
       subroutine tapenade_ar_d(model, n, nd, v, vd, t, td, arval, arvald)
          import pr, ArModelTapenade
@@ -33,7 +33,7 @@ module yaeos__tapenade_ar_api
          real(pr), intent(in) :: n(:), v, t
          real(pr), intent(in) :: nd(:), vd, td
          real(pr), intent(out) :: arval, arvald
-      end subroutine
+      end subroutine tapenade_ar_d
 
       subroutine tapenade_ar_b(model, n, nb, v, vb, t, tb, arval, arvalb)
          import pr, ArModelTapenade
@@ -42,7 +42,7 @@ module yaeos__tapenade_ar_api
          real(pr) :: arvalb
          real(pr) :: nb(:), vb, tb
          real(pr) :: arval
-      end subroutine
+      end subroutine tapenade_ar_b
 
       subroutine tapenade_ar_d_b(model, &
          n, nb, nd, ndb, v, vb, vd, vdb, t, tb, td, tdb, &
@@ -50,6 +50,7 @@ module yaeos__tapenade_ar_api
          import pr, ArModelTapenade
          class(ArModelTapenade), intent(in) :: model
          real(pr), intent(in) :: n(:), v, t
+         real(pr) :: arval
 
          real(pr), intent(in) :: nd(:), vd, td
          real(pr) :: arvald
@@ -59,9 +60,7 @@ module yaeos__tapenade_ar_api
 
          real(pr) :: ndb(:), vdb, tdb
          real(pr) :: arvaldb
-
-         real(pr) :: arval
-      end subroutine
+      end subroutine tapenade_ar_d_b
 
       subroutine tapenade_ar_d_d(model, n, nd, v, vd0, vd, t, td0, td, &
          arval, arvald0, arvald, arvaldd)
@@ -71,7 +70,7 @@ module yaeos__tapenade_ar_api
          real(pr), intent(in) :: vd0, td0
          real(pr), intent(in) :: nd(:), vd, td
          real(pr), intent(out) :: arval, arvald0, arvald, arvaldd
-      end subroutine
+      end subroutine tapenade_ar_d_d
    end interface
 
 contains
@@ -80,7 +79,6 @@ contains
       self, n, v, t, Ar, ArV, ArT, ArTV, ArV2, ArT2, Arn, ArVn, ArTn, Arn2 &
       )
       !! Residual Helmholtz model generic interface
-      !! FIXED: Handles all derivative combinations efficiently with no redundant calls
       class(ArModelTapenade), intent(in) :: self
       real(pr), intent(in) :: n(:)
       real(pr), intent(in) :: v, t
@@ -88,220 +86,163 @@ contains
       real(pr), optional, dimension(size(n)), intent(out) :: Arn, ArVn, ArTn
       real(pr), optional, intent(out) :: Arn2(size(n), size(n))
 
-      ! Working arrays
       real(pr) :: nb(size(n)), nd(size(n)), ndb(size(n))
       real(pr) :: vb, vd, vdb, vd0
       real(pr) :: tb, td, tdb, td0
       real(pr) :: arval, arvalb, arvald, arvaldb, arvald0, arvaldd
 
-      ! Flags to track what's been computed
-      logical :: computed_arn, computed_arv, computed_art
       integer :: i, nc
+      logical :: need_Arn2, need_ArVn, need_ArTn
+      logical :: need_ArV2, need_ArT2, need_ArTV
+      logical :: need_grad, need_val
 
       nc = size(n)
 
-      ! =========================================================================
-      ! PATH 1: Arn2 requested - use forward-on-backward loop
-      ! This efficiently computes: Arn2, Arn, ArV, ArT, ArVn, ArTn
-      ! =========================================================================
-      if (present(Arn2)) then
-         computed_arn = .false.
-         computed_arv = .false.
-         computed_art = .false.
+      ! Initialize tracking flags based on requested arguments
+      need_Arn2 = present(Arn2)
+      need_ArVn = present(ArVn)
+      need_ArTn = present(ArTn)
+      need_ArV2 = present(ArV2)
+      need_ArT2 = present(ArT2)
+      need_ArTV = present(ArTV)
+      need_grad = present(Arn) .or. present(ArV) .or. present(ArT)
+      need_val  = present(Ar)
 
+      ! 1. Hessian matrix w.r.t composition (Uses backward pass: does NOT yield primal Ar)
+      if (need_Arn2) then
          do i = 1, nc
             call reset_vars
-
-            ! Set up forward direction (component i)
-            nd(i) = 1
-
-            ! Seed for backward sweep (computes second derivatives)
             arvaldb = 1
-
-            call self%ar_d_b(&
+            nd(i) = 1
+            call self%ar_d_b( &
                n, nb, nd, ndb, &
                v, vb, vd, vdb, &
                t, tb, td, tdb, &
-               arval, arvalb, arvald, arvaldb &
-            )
+               arval, arvalb, arvald, arvaldb)
 
-            ! Extract second derivatives (Hessian)
-            ! Arn2(i, :) = ndb
             Arn2(i, :) = nb
 
-            ! Save first derivatives on first iteration (identical for all i)
+            ! Extract free cross-derivatives
+            if (need_ArVn) ArVn(i) = vb
+            if (need_ArTn) ArTn(i) = tb
+
+            ! Gradients are identical across all iterations of this loop
             if (i == 1) then
-               if (present(Arn)) then
-                  Arn = nb
-                  computed_arn = .true.
-               end if
-               if (present(ArV)) then
-                  ArV = vb
-                  computed_arv = .true.
-               end if
-               if (present(ArT)) then
-                  ArT = tb
-                  computed_art = .true.
-               end if
+               if (present(Arn)) Arn = ndb
+               if (present(ArV)) ArV = vdb
+               if (present(ArT)) ArT = tdb
+               need_grad = .false.
             end if
-
-            ! Save mixed second derivatives (optimized: avoid redundant calls)
-            if (present(ArVn)) ArVn(i) = vdb
-            if (present(ArTn)) ArTn(i) = tdb
          end do
+         need_ArVn = .false.
+         need_ArTn = .false.
+      end if
 
-      ! =========================================================================
-      ! PATH 2: Only Arn requested (no Arn2) - use simple reverse mode
-      ! =========================================================================
-      else if (present(Arn)) then
+      ! 2. Cross derivative w.r.t Volume and Composition (Uses backward pass)
+      if (need_ArVn) then
+         call reset_vars
+         arvaldb = 1
+         vd = 1
+         call self%ar_d_b( &
+            n, nb, nd, ndb, &
+            v, vb, vd, vdb, &
+            t, tb, td, tdb, &
+            arval, arvalb, arvald, arvaldb)
+
+         ArVn = nb
+
+         ! Free byproducts from vd=1 sweep
+         if (need_ArV2) then
+            ArV2 = vb
+            need_ArV2 = .false.
+         end if
+         if (need_ArTV) then
+            ArTV = tb
+            need_ArTV = .false.
+         end if
+         if (need_grad) then
+            if (present(Arn)) Arn = ndb
+            if (present(ArV)) ArV = vdb
+            if (present(ArT)) ArT = tdb
+            need_grad = .false.
+         end if
+      end if
+
+      ! 3. Cross derivative w.r.t Temperature and Composition (Uses backward pass)
+      if (need_ArTn) then
+         call reset_vars
+         arvaldb = 1
+         td = 1
+         call self%ar_d_b( &
+            n, nb, nd, ndb, &
+            v, vb, vd, vdb, &
+            t, tb, td, tdb, &
+            arval, arvalb, arvald, arvaldb)
+
+         ArTn = nb
+
+         ! Free byproducts from td=1 sweep
+         if (need_ArT2) then
+            ArT2 = tb
+            need_ArT2 = .false.
+         end if
+         if (need_ArTV) then
+            ArTV = vb  ! vb is d2Ar/(dt*dv)
+            need_ArTV = .false.
+         end if
+         if (need_grad) then
+            if (present(Arn)) Arn = ndb
+            if (present(ArV)) ArV = vdb
+            if (present(ArT)) ArT = tdb
+            need_grad = .false.
+         end if
+      end if
+
+      ! 4. Pure Gradients (Uses backward pass: does NOT yield primal Ar)
+      if (need_grad) then
          call reset_vars
          arvalb = 1
          call self%ar_b(n, nb, v, vb, t, tb, arval, arvalb)
-         Arn = nb
-         computed_arn = .true.
+         if (present(Arn)) Arn = nb
+         if (present(ArV)) ArV = vb
+         if (present(ArT)) ArT = tb
+      end if
 
-         ! Grab first derivatives as byproduct
-         if (present(ArV)) then
-            ArV = vb
-            computed_arv = .true.
-         end if
-         if (present(ArT)) then
-            ArT = tb
-            computed_art = .true.
-         end if
-
-      ! =========================================================================
-      ! PATH 3: No composition derivatives, but V,T first derivatives needed
-      ! =========================================================================
-      else if ((present(ArV) .or. present(ArT)) .and. &
-               .not. present(ArVn) .and. .not. present(ArTn)) then
+      ! 5. Scalar 2nd derivatives (Uses forward sweep: DOES yield primal Ar)
+      if (need_ArV2) then
          call reset_vars
-         arvalb = 1
-         call self%ar_b(n, nb, v, vb, t, tb, arval, arvalb)
-
-         if (present(ArV)) then
-            ArV = vb
-            computed_arv = .true.
-         end if
-         if (present(ArT)) then
-            ArT = tb
-            computed_art = .true.
-         end if
-      else
-         computed_arn = .false.
-         computed_arv = .false.
-         computed_art = .false.
+         vd = 1; vd0 = 1
+         call self%ar_d_d(n, nd, v, vd0, vd, t, td0, td, arval, arvald0, arvald, arvaldd)
+         ArV2 = arvaldd
       end if
 
-      ! =========================================================================
-      ! Mixed second derivatives (ArVn, ArTn) if not already computed above
-      ! =========================================================================
-      if (present(ArVn) .and. .not. present(Arn2)) then
-         ArVn = get_ArnX("V")
+      if (need_ArT2) then
+         call reset_vars
+         td = 1; td0 = 1
+         call self%ar_d_d(n, nd, v, vd0, vd, t, td0, td, arval, arvald0, arvald, arvaldd)
+         ArT2 = arvaldd
       end if
 
-      if (present(ArTn) .and. .not. present(Arn2)) then
-         ArTn = get_ArnX("T")
+      if (need_ArTV) then
+         call reset_vars
+         vd = 1; td0 = 1
+         call self%ar_d_d(n, nd, v, vd0, vd, t, td0, td, arval, arvald0, arvald, arvaldd)
+         ArTV = arvaldd
       end if
 
-      ! =========================================================================
-      ! Pure second derivatives (ArV2, ArT2, ArTV) using forward-on-forward
-      ! =========================================================================
-      if (present(ArTV)) ArTV = get_dArdX2("TV")
-      if (present(ArT2)) ArT2 = get_dArdX2("T2")
-      if (present(ArV2)) ArV2 = get_dArdX2("V2")
-
-      ! =========================================================================
-      ! The function value itself (if requested and not yet computed)
-      ! =========================================================================
-      if (present(Ar)) then
-         ! Ar is computed in most paths; if not, compute it now
-         if (.not. (present(Arn2) .or. present(Arn) .or. &
-                     (present(ArV) .and. computed_arv) .or. &
-                     (present(ArT) .and. computed_art))) then
-            call reset_vars
-            call self%ar(n, v, t, arval)
-         end if
+      ! 6. Pure Evaluation (Triggered if Ar is needed and NO forward sweep caught it)
+      if (need_val) then
+         call reset_vars
+         call self%ar(n, v, t, arval)
          Ar = arval
       end if
 
    contains
-
       subroutine reset_vars
-         !! Reset all working variables
-         nb = 0
-         nd = 0
-         ndb = 0
-
-         vb = 0
-         vd = 0
-         vd0 = 0
-         vdb = 0
-
-         tb = 0
-         td = 0
-         td0 = 0
-         tdb = 0
-
-         arval = 0
-         arvalb = 0
-         arvald = 0
-         arvald0 = 0
-         arvaldb = 0
-      end subroutine
-
-      function get_dArdX2(var)
-         !! Compute pure second derivatives using forward-on-forward AD
-         !! Computes: ArV2, ArT2, ArTV
-         character(len=*), intent(in) :: var
-         real(pr) :: get_dArdX2
-
-         call reset_vars
-
-         select case (var)
-         case ("TV")
-            vd = 1
-            td0 = 1
-         case ("V2")
-            vd = 1
-            vd0 = 1
-         case ("T2")
-            td = 1
-            td0 = 1
-         end select
-
-         call self%ar_d_d(&
-            n, nd, v, vd0, vd, t, td0, td, &
-            arval, arvald0, arvald, arvaldd &
-         )
-         get_dArdX2 = arvaldd
-      end function
-
-      function get_ArnX(var)
-         !! Compute mixed second derivatives: ArVn or ArTn
-         !! Uses forward-on-backward AD
-         character(len=*), intent(in) :: var
-         real(pr) :: get_ArnX(size(n))
-
-         call reset_vars
-
-         arvaldb = 1
-         select case (var)
-         case ("V")
-            vd = 1
-         case ("T")
-            td = 1
-         end select
-
-         call self%ar_d_b(&
-            n, nb, nd, ndb, &
-            v, vb, vd, vdb, &
-            t, tb, td, tdb, &
-            arval, arvalb, arvald, arvaldb &
-         )
-         get_ArnX = nb
-      end function
-
-   end subroutine
-
-end module
+         nb=0; nd=0; ndb=0
+         vb=0; vd=0; vd0=0; vdb=0
+         tb=0; td=0; td0=0; tdb=0
+         arval=0; arvalb=0; arvald=0; arvald0=0; arvaldb=0; arvaldd=0
+      end subroutine reset_vars
+   end subroutine residual_helmholtz
+end module yaeos__tapenade_ar_api
